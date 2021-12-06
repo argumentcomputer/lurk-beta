56ee49d36436d5c53c2f1473d115e7b0d9c973f1
Anders Christiansen Sørby <anders@sorby.xyz>
2021-12-06 23:00:48 +0100

    Renable flake check in CI

--- flake.nix
+++ flake.nix
@@ -26,10 +26,10 @@
     }:
     let
       supportedSystems = [
-        # "aarch64-linux"
+        "aarch64-linux"
         # "aarch64-darwin"
         "i686-linux"
-        "x86_64-darwin"
+        # "x86_64-darwin"
         "x86_64-linux"
       ];
     in
@@ -52,12 +52,13 @@
         copySources = [ "examples" "src" ];
         copyBins = true;
       };
-    in
-    {
       packages = {
         ${crateName} = project;
         inherit lurk-example;
       };
+    in
+    {
+      inherit packages;
       checks.${crateName} = project.override { doCheck = true; };
 
       defaultPackage = self.packages.${system}.${crateName};
