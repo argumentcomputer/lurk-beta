cf059439ae1ab6f037871f9f425572cfeab57bb4
porcuquine <1746729+porcuquine@users.noreply.github.com>
2022-02-02 13:36:31 -0800

    Proof aggregation. (#29)
    * Proof aggregation.
    
    * Update patches.
    
    * Revert unneeded public Ptr fields.
    
    Co-authored-by: porcuquine <porcuquine@users.noreply.github.com>

--- src/proof.rs
+++ src/proof.rs
@@ -1,11 +1,16 @@
 use bellperson::util_cs::test_cs::TestConstraintSystem;
 use bellperson::{
-    groth16::{self, verify_proof},
+    groth16::{
+        self,
+        aggregate::{aggregate_proofs, setup_fake_srs, AggregateProof, GenericSRS, ProverSRS},
+        verify_proof,
+    },
     Circuit, SynthesisError,
 };
 use blstrs::{Bls12, Scalar as Fr};
-use once_cell::sync::OnceCell;
-use pairing_lib::Engine;
+use ff::PrimeField;
+use once_cell::sync::{Lazy, OnceCell};
+use pairing_lib::{Engine, MultiMillerLoop};
 use rand::{RngCore, SeedableRng};
 use rand_xorshift::XorShiftRng;
 
@@ -13,19 +18,62 @@ use crate::circuit::CircuitFrame;
 use crate::eval::{Evaluator, Frame, Witness, IO};
 use crate::store::{Ptr, ScalarPointer, Store};
 
+use std::env;
+use std::fs::File;
+use std::io;
+
 const DUMMY_RNG_SEED: [u8; 16] = [
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 ];
 
+pub static INNER_PRODUCT_SRS: Lazy<GenericSRS<Bls12>> = Lazy::new(|| load_srs().unwrap());
 static FRAME_GROTH_PARAMS: OnceCell<groth16::Parameters<Bls12>> = OnceCell::new();
+const MAX_FAKE_SRS_SIZE: usize = 2 << 20;
+
+// If you don't have a real SnarkPack SRS symlinked, generate a fake one.
+// Don't use this in production!
+const FALLBACK_TO_FAKE_SRS: bool = true;
+
+type SequentialProofs<'a, E, IO, Witness> = Vec<(CircuitFrame<'a, IO, Witness>, groth16::Proof<E>)>;
+type SequentialCS<IO, Witness> = Vec<(Frame<IO, Witness>, TestConstraintSystem<Fr>)>;
+type FrameProofs<'a, E, IO, Witness> = (Proof<'a, E, CircuitFrame<'a, IO, Witness>>, Vec<Vec<Fr>>);
+
+fn load_srs() -> Result<GenericSRS<Bls12>, io::Error> {
+    let path = env::current_dir()?.join("params/v28-fil-inner-product-v1.srs");
+    let mut rng = rand::thread_rng();
+    let f = File::open(path);
+
+    match f {
+        Ok(mut f) => GenericSRS::read(&mut f),
+        Err(e) => {
+            if FALLBACK_TO_FAKE_SRS {
+                Ok(setup_fake_srs::<Bls12, _>(&mut rng, MAX_FAKE_SRS_SIZE))
+            } else {
+                Err(e)
+            }
+        }
+    }
+}
 
 pub trait Provable {
     fn public_inputs(&self, store: &Store) -> Vec<Fr>;
+    fn extend_transcript(&self, transcript: &mut Vec<u8>, store: &Store);
 }
 
-#[derive(Clone)]
-pub struct Proof<E: Engine> {
-    groth16_proof: groth16::Proof<E>,
+#[allow(dead_code)]
+pub struct Proof<'a, E: Engine + MultiMillerLoop, F: Provable>
+where
+    <E as Engine>::Gt: blstrs::Compress,
+{
+    frame_proofs: SequentialProofs<'a, E, IO, Witness>,
+    aggregated_proof: AggregateProof<E>,
+    // FIXME: We shouldn't really include the transcript data in the proof,
+    // but we also will not ultimately be including all the intermediate public inputs either.
+    // Meanwhile, this is a simplification to allow verification.
+    // Once we can do input/instance aggregation with SnarkPack,
+    // get rid of this (and also the frame proofs.
+    transcript_include: Vec<u8>,
+    frames: Vec<F>,
 }
 
 impl<W> Provable for CircuitFrame<'_, IO, W> {
@@ -46,6 +94,25 @@ impl<W> Provable for CircuitFrame<'_, IO, W> {
         }
 
         inputs
+    }
+
+    fn extend_transcript(&self, transcript: &mut Vec<u8>, store: &Store) {
+        // TODO: Refactor to share code with `public_inputs` to avoid allocation.
+        let inputs = self.public_inputs(store);
+
+        for input in inputs {
+            let bytes: [u8; 32] = input.to_repr();
+            transcript.extend(bytes);
+        }
+    }
+}
+
+impl<T: PartialEq + std::fmt::Debug, W> CircuitFrame<'_, T, W> {
+    pub fn precedes(&self, maybe_next: &Self) -> bool {
+        let sequential = self.i.unwrap() + 1 == maybe_next.i.unwrap();
+        let io_match = self.output == maybe_next.input;
+
+        sequential && io_match
     }
 }
 
@@ -95,40 +162,72 @@ impl<'a> CircuitFrame<'a, IO, Witness> {
         self,
         params: Option<&groth16::Parameters<Bls12>>,
         mut rng: R,
-    ) -> Result<Proof<Bls12>, SynthesisError> {
-        Ok(Proof {
-            groth16_proof: Self::generate_groth16_proof(self, params, &mut rng)?,
-        })
+    ) -> Result<groth16::Proof<Bls12>, SynthesisError> {
+        Self::generate_groth16_proof(self, params, &mut rng)
     }
 
     #[allow(clippy::needless_collect)]
     pub fn outer_prove<R: RngCore + Clone>(
         params: &groth16::Parameters<Bls12>,
+        srs: ProverSRS<Bls12>,
         expr: Ptr,
         env: Ptr,
-        store: &mut Store,
+        store: &'a mut Store,
         limit: usize,
         rng: R,
-    ) -> Result<SequentialProofs<Bls12, IO, Witness>, SynthesisError> {
+    ) -> Result<FrameProofs<'a, Bls12, IO, Witness>, SynthesisError> {
         // FIXME: optimize execution order
         let mut evaluator = Evaluator::new(expr, env, store, limit);
         let initial = evaluator.initial();
-        let frames = evaluator.iter().collect::<Vec<_>>();
+        let mut transcript_include = Vec::new();
+        let mut frames = evaluator.iter().collect::<Vec<_>>();
+        let mut proofs = Vec::with_capacity(frames.len());
+        let mut circuit_frames = Vec::with_capacity(frames.len());
+
+        let mut public_inputs = Vec::with_capacity(frames.len());
         store.hydrate_scalar_cache();
 
-        // FIXME: Don't clone the RNG.
-        let res = frames
-            .into_iter()
-            .map(|frame| {
-                (
-                    frame.clone(),
-                    CircuitFrame::from_frame(initial.clone(), frame, store)
-                        .prove(Some(params), rng.clone())
-                        .unwrap(),
-                )
-            })
-            .collect::<Vec<_>>();
-        Ok(res)
+        // NOTE: frame_proofs are not really needed, but having them helps with
+        // testing and building confidence as we work up to fully succinct proofs.
+        // Once these are removed a lot of the cloning and awkwardness of assembling
+        // results here can be eliminated.
+        let mut frame_proofs = Vec::with_capacity(frames.len());
+
+        for frame in &frames {
+            let circuit_frame = CircuitFrame::from_frame(initial.clone(), frame.clone(), store);
+            public_inputs.push(circuit_frame.clone().public_inputs(store));
+            circuit_frames.push(circuit_frame.clone());
+
+            // FIXME: Don't clone the RNG (?).
+            let proof = circuit_frame
+                .clone()
+                .prove(Some(params), rng.clone())
+                .unwrap();
+            proofs.push(proof.clone());
+
+            circuit_frame.extend_transcript(&mut transcript_include, store);
+
+            frame_proofs.push((circuit_frame, proof));
+        }
+
+        while proofs.len().count_ones() != 1 {
+            // Pad proofs and frames to a power of 2.
+            proofs.push(proofs[0].clone());
+            frames.push(frames[0].clone());
+            public_inputs.push(public_inputs[0].clone());
+        }
+
+        let aggregated_proof = aggregate_proofs(&srs, &transcript_include, proofs.as_slice())?;
+
+        Ok((
+            Proof {
+                frame_proofs,
+                aggregated_proof,
+                transcript_include,
+                frames: circuit_frames,
+            },
+            public_inputs,
+        ))
     }
 
     #[allow(clippy::needless_collect)]
@@ -158,20 +257,17 @@ impl<'a> CircuitFrame<'a, IO, Witness> {
     }
 }
 
-type SequentialProofs<E, IO, Witness> = Vec<(Frame<IO, Witness>, Proof<E>)>;
-type SequentialCS<IO, Witness> = Vec<(Frame<IO, Witness>, TestConstraintSystem<Fr>)>;
-
 #[allow(dead_code)]
 fn verify_sequential_groth16_proofs(
-    proofs: SequentialProofs<Bls12, IO, Witness>,
+    frame_proofs: SequentialProofs<Bls12, IO, Witness>,
     vk: &groth16::VerifyingKey<Bls12>,
-    store: &Store,
 ) -> Result<bool, SynthesisError> {
-    let previous_frame: Option<&Frame<IO, Witness>> = None;
+    let previous_frame: Option<&CircuitFrame<IO, Witness>> = None;
     let pvk = groth16::prepare_verifying_key(vk);
-    let initial = proofs[0].0.input.clone();
+    let initial = frame_proofs[0].0.input.clone();
+    let retained = frame_proofs[frame_proofs.len() - 1].0.initial.clone();
 
-    for (i, (frame, proof)) in proofs.into_iter().enumerate() {
+    for (i, (frame, proof)) in frame_proofs.into_iter().enumerate() {
         dbg!(i);
         if let Some(prev) = previous_frame {
             if !prev.precedes(&frame) {
@@ -179,16 +275,12 @@ fn verify_sequential_groth16_proofs(
             }
         }
 
-        if !CircuitFrame::from_frame(initial.clone(), frame, store)
-            .verify_groth16_proof(&pvk, proof.clone())?
-        {
+        if !frame.verify_groth16_proof(&pvk, proof.clone())? {
             return Ok(false);
         }
     }
 
-    // TODO: Check that the last frame's initial field equals the first frames input.
-
-    Ok(true)
+    Ok(initial == retained)
 }
 
 #[allow(dead_code)]
@@ -241,11 +333,11 @@ impl CircuitFrame<'_, IO, Witness> {
     pub fn verify_groth16_proof(
         self,
         pvk: &groth16::PreparedVerifyingKey<Bls12>,
-        p: Proof<Bls12>,
+        proof: groth16::Proof<Bls12>,
     ) -> Result<bool, SynthesisError> {
         let inputs = self.public_inputs(self.store);
 
-        verify_proof(pvk, &p.groth16_proof, &inputs)
+        verify_proof(pvk, &proof, &inputs)
     }
 }
 
@@ -253,7 +345,9 @@ impl CircuitFrame<'_, IO, Witness> {
 mod tests {
     use super::*;
     use crate::eval::empty_sym_env;
+    use bellperson::groth16::aggregate::verify_aggregate_proof;
     use bellperson::util_cs::{metric_cs::MetricCS, Comparable, Delta};
+    use rand::rngs::OsRng;
 
     const DEFAULT_CHECK_GROTH16: bool = false;
 
@@ -266,17 +360,27 @@ mod tests {
         limit: usize,
         debug: bool,
     ) {
-        let rng = rand::thread_rng();
+        let rng = OsRng;
 
         let mut s = Store::default();
+        let expected_result = expected_result(&mut s);
+
         let expr = s.read(source).unwrap();
 
         let groth_params = CircuitFrame::groth_params().unwrap();
 
-        let proofs = if check_groth16 {
+        let pvk = groth16::prepare_verifying_key(&groth_params.vk);
+
+        let size = 32;
+        let (srs_pk, srs_vk) = INNER_PRODUCT_SRS.specialize(size);
+
+        let e = empty_sym_env(&s);
+
+        let proof = if check_groth16 {
             Some(
                 CircuitFrame::outer_prove(
                     &groth_params,
+                    srs_pk,
                     expr.clone(),
                     empty_sym_env(&s),
                     &mut s,
@@ -289,21 +393,41 @@ mod tests {
             None
         };
 
-        let expected_result = expected_result(&mut s);
-        let constraint_systems = if check_constraint_systems {
-            Some(CircuitFrame::outer_synthesize(expr, empty_sym_env(&s), &mut s, limit).unwrap())
-        } else {
-            None
-        };
-
-        if let Some(proofs) = proofs {
+        if let Some((proof, public_inputs)) = proof {
+            let frame_proofs = proof.frame_proofs.clone();
             if !debug {
-                assert_eq!(expected_iterations, proofs.len());
-                assert_eq!(expected_result, proofs[proofs.len() - 1].0.output.expr);
+                assert_eq!(expected_iterations, frame_proofs.len());
+                assert_eq!(
+                    expected_result,
+                    frame_proofs[frame_proofs.len() - 1]
+                        .0
+                        .output
+                        .as_ref()
+                        .unwrap()
+                        .expr
+                );
             }
             let proofs_verified =
-                verify_sequential_groth16_proofs(proofs, &groth_params.vk, &s).unwrap();
+                verify_sequential_groth16_proofs(proof.frame_proofs, &groth_params.vk).unwrap();
             assert!(proofs_verified);
+
+            let aggregate_proof_verified = verify_aggregate_proof(
+                &srs_vk,
+                &pvk,
+                rng.clone(),
+                &public_inputs,
+                &proof.aggregated_proof,
+                &proof.transcript_include,
+            )
+            .unwrap();
+
+            assert!(aggregate_proof_verified);
+        };
+
+        let constraint_systems = if check_constraint_systems {
+            Some(CircuitFrame::outer_synthesize(expr, e, &mut s, limit).unwrap())
+        } else {
+            None
         };
 
         if let Some(cs) = constraint_systems {
