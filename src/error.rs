8410d6919b8d4b1899bb70ca2d43c449c455699b
Jeff Weiss <me@jweiss.com>
2022-12-23 11:33:19 -0300

    Eliminate barely used LurkError::Reduce variant
    Errors during reduce will be dealt with later, with an explicit notion
    of a provable vs non-provable error.

--- src/error.rs
+++ src/error.rs
@@ -18,8 +18,6 @@ pub enum ProofError {
 pub enum LurkError {
     #[error("Evaluation error: {0}")]
     Eval(String),
-    #[error("Reduction error: {0}")]
-    Reduce(String),
     #[error("Lookup error: {0}")]
     Store(#[from] store::Error),
     #[error("Parser error: {0}")]
