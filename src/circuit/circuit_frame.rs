df3647c7fa39647366a62e5ca02cee7be28dca2e
emmorais <eduardo.morais@gmail.com>
2023-01-26 10:38:09 -0300

    Remove dead code

--- src/circuit/circuit_frame.rs
+++ src/circuit/circuit_frame.rs
@@ -4690,10 +4690,6 @@ pub fn to_unsigned_integer_helper<F: LurkField, CS: ConstraintSystem<F>>(
     // field element = pow(2, size).q + r
     let sum = add(&mut cs.namespace(|| "sum remainder"), &product, &r_num)?;
     equal(&mut cs, || "check unsigned decomposition", &sum, field_elem);
-    //enforce_true(
-    //    &mut cs.namespace(|| "enforce decomposition of unsigned integer"),
-    //    &unsigned_decomp,
-    //)?;
     let r_bits = &field_elem_bits[0..size.num_bits() as usize];
     enforce_pack(
         &mut cs.namespace(|| "enforce unsigned pack"),
