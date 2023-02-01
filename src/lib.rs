0873ce07259e1bd28f907189e1de6d0142e8e742
John C. Burnham <jcb@johnchandlerburnham.com>
2023-01-31 19:35:33 -0500

    fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -29,45 +29,37 @@ pub mod writer;
 mod error;
 mod num;
 pub use num::Num;
-pub use sym::{
-  Sym,
-  Symbol,
-};
+pub use sym::{Sym, Symbol};
 pub use uint::UInt;
 
 pub const TEST_SEED: [u8; 16] = [
-  0x62, 0x59, 0x5d, 0xbe, 0x3d, 0x76, 0x3d, 0x8d, 0xdb, 0x17, 0x32, 0x37, 0x06,
-  0x54, 0xe5, 0xbc,
+    0x62, 0x59, 0x5d, 0xbe, 0x3d, 0x76, 0x3d, 0x8d, 0xdb, 0x17, 0x32, 0x37, 0x06, 0x54, 0xe5, 0xbc,
 ];
 
 #[cfg(test)]
 pub mod test {
-  use quickcheck::Gen;
-  use rand::Rng;
+    use quickcheck::Gen;
+    use rand::Rng;
 
-  // This is a useful testing utility for generating Arbitrary instances of
-  // enums, by providing generators for each variant, plus a frequency weight
-  // for how often to choose that variant. It's included in lib::test to make
-  // it easier to import in the test modules of specific submodules.
-  pub fn frequency<T, F: Fn(&mut Gen) -> T>(
-    g: &mut Gen,
-    gens: Vec<(i64, F)>,
-  ) -> T {
-    if gens.iter().any(|(v, _)| *v < 0) {
-      panic!("Negative weight");
+    // This is a useful testing utility for generating Arbitrary instances of
+    // enums, by providing generators for each variant, plus a frequency weight
+    // for how often to choose that variant. It's included in lib::test to make
+    // it easier to import in the test modules of specific submodules.
+    pub fn frequency<T, F: Fn(&mut Gen) -> T>(g: &mut Gen, gens: Vec<(i64, F)>) -> T {
+        if gens.iter().any(|(v, _)| *v < 0) {
+            panic!("Negative weight");
+        }
+        let sum: i64 = gens.iter().map(|x| x.0).sum();
+        let mut rng = rand::thread_rng();
+        let mut weight: i64 = rng.gen_range(1..=sum);
+        // let mut weight: i64 = g.rng.gen_range(1, sum);
+        for gen in gens {
+            if weight - gen.0 <= 0 {
+                return gen.1(g);
+            } else {
+                weight -= gen.0;
+            }
+        }
+        panic!("Calculation error for weight = {}", weight);
     }
-    let sum: i64 = gens.iter().map(|x| x.0).sum();
-    let mut rng = rand::thread_rng();
-    let mut weight: i64 = rng.gen_range(1..=sum);
-    // let mut weight: i64 = g.rng.gen_range(1, sum);
-    for gen in gens {
-      if weight - gen.0 <= 0 {
-        return gen.1(g);
-      }
-      else {
-        weight -= gen.0;
-      }
-    }
-    panic!("Calculation error for weight = {}", weight);
-  }
 }
