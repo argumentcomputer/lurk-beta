6e6469ebc6a076ab119621c6fed9551c053f5e96
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-03-10 09:27:31 -0300

    replace the foo store by a more realistic one

--- tests/light-data-tests.rs
+++ tests/light-data-tests.rs
@@ -8,16 +8,20 @@ pub mod tests {
     #[test]
     // The following store was created with the following Lean code:
     //  def foo : IO Unit := do
-    //    let store : Lurk.Store := .ofList [
-    //      (⟨.nil,  .ofNat 0⟩, some $ .nil),
-    //      (⟨.cons, .ofNat 1⟩, some $ .cons ⟨.nil, .ofNat 0⟩ ⟨.nil, .ofNat 1⟩),
-    //      (⟨.sym,  .ofNat 2⟩, some $ .symNil),
-    //      (⟨.str,  .ofNat 3⟩, some $ .strNil),
-    //      (⟨.num,  .ofNat 4⟩, some $ .num (.ofNat 42)),
-    //      (⟨.char, .ofNat 5⟩, some $ .char (.ofNat 37)),
-    //      (⟨.comm, .ofNat 6⟩, some $ .comm (.ofNat 37) ⟨.sym, .ofNat 2⟩)
-    //    ] _
-    //    let ld : LightData := store
+    //    let ldonNil := LDON.nil
+    //    let ldonNum1 := LDON.num (.ofNat 0)
+    //    let ldonNum2 := LDON.num (.ofNat 256)
+    //    let ldonNum3 := LDON.num (.ofNat (2 * UInt64.size))
+    //    let ldonStr := LDON.str "hello, how are you?"
+    //    let ldonCons1 := LDON.cons ldonNil ldonNil
+    //    let ldonCons2 := LDON.cons ldonNum1 (LDON.cons ldonNil ldonStr)
+    //    let ldonCons3 := LDON.cons ldonNum1 (LDON.cons ldonNil ldonNum2)
+    //    let ldonCons4 := LDON.cons ldonCons3 ldonCons3
+    //    let ldons := [ldonNil, ldonNum1, ldonNum2, ldonNum3, ldonStr,
+    //      ldonCons1, ldonCons2, ldonCons3, ldonCons4]
+    //    let stt := ldons.foldl (init := default) fun acc ldon =>
+    //      let (_, acc) := ldon.commit acc; acc
+    //    let ld : LightData := stt.exprs
     //    IO.FS.writeBinFile ⟨"foo.store"⟩ ld.toByteArray
     fn test_light_store_deserialization () {
         let bytes = fs::read("foo.store").unwrap();
