b412e8e5b3755307dd2645be64af95cb49022907
porcuquine <porcuquine@users.noreply.github.com>
2023-03-23 18:59:43 +0000

    deploy: 108b3d2a7d4435ba6f2c36636b747db596af36e5

--- docs/lurk/proof/nova/sidebar-items.js
+++ docs/lurk/proof/nova/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Proof"],"fn":["public_params"],"struct":["NovaProver"],"type":["C1","C2","EE1","EE2","G1","G2","PublicParams","S1","S2","SS1","SS2"]};
+window.SIDEBAR_ITEMS = {"enum":["Proof"],"fn":["public_params"],"struct":["NovaProver","PublicParams"],"type":["C1","C2","EE1","EE2","G1","G2","NovaPublicParams","S1","S2","SS1","SS2"]};
