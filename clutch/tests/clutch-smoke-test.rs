0fd922944a69e1c9e97a9fc2ad536f6f88cf78a4
porcuquine <1746729+porcuquine@users.noreply.github.com>
2023-03-30 11:49:24 -0700

    Add smoke test for clutch. (#323)
    Co-authored-by: porcuquine <porcuquine@users.noreply.github.com>

---
+++ clutch/tests/clutch-smoke-test.rs
@@ -0,0 +1,14 @@
+use assert_cmd::prelude::*;
+use std::process::Command;
+
+fn lurkrs_cmd() -> std::process::Command {
+    Command::cargo_bin("clutch").unwrap()
+}
+
+#[test]
+fn test_help_command() {
+    let mut cmd = lurkrs_cmd();
+
+    cmd.arg("--help");
+    cmd.assert().success();
+}
