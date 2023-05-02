da08ed14ab9704adc0894b2a0e25528c69a31eb3
Matej Penciak <matej.penciak@gmail.com>
2023-05-02 09:44:18 -0400

    clean up ugly code

--- examples/sha256.rs
+++ examples/sha256.rs
@@ -8,6 +8,7 @@ use bellperson::{ConstraintSystem, SynthesisError};
 use bellperson::gadgets::num::AllocatedNum;
 use bellperson::gadgets::boolean::{Boolean, AllocatedBit};
 use bellperson::gadgets::num::Num as BNum;
+use itertools::enumerate;
 use lurk::circuit::gadgets::data::GlobalAllocations;
 use lurk::proof::nova::{NovaProver, public_params};
 // use bellperson::gadgets::Assignment;
@@ -56,21 +57,18 @@ impl<F: LurkField> CoCircuit<F> for Sha256Coprocessor<F> {
 
         bits.reverse();
 
-        let num1 = make_u64_from_bits(&mut cs.namespace(|| "num1"), &bits[0..64])?;
-        let num2 = make_u64_from_bits(&mut cs.namespace(|| "num2"), &bits[64..128])?;
-        let num3 = make_u64_from_bits(&mut cs.namespace(|| "num3"), &bits[128..192])?;
-        let num4 = make_u64_from_bits(&mut cs.namespace(|| "num4"), &bits[192..256])?;
 
-        // dbg!(num1.fetch_and_write_str(store), num2.fetch_and_write_str(store), num3.fetch_and_write_str(store), num4.fetch_and_write_str(store));
+        let nums: Vec<AllocatedPtr<F>> = (0..4).map(
+            |i| make_u64_from_bits(cs.namespace(|| format!("num{i}")), &bits [(64 * i) .. (64 * (i + 1))]).unwrap()
+        ).collect();
 
-        let result_ptr: &AllocatedPtr<F> = &g.nil_ptr;
+        let mut result_ptr: AllocatedPtr<F> = g.nil_ptr.clone();
 
-        let result_ptr1 = AllocatedPtr::construct_cons(cs.namespace(|| "limb_1"), g, store, &num1, &result_ptr)?;
-        let result_ptr2 = AllocatedPtr::construct_cons(cs.namespace(|| "limb_2"), g, store, &num2, &result_ptr1)?;
-        let result_ptr3 = AllocatedPtr::construct_cons(cs.namespace(|| "limb_3"), g, store, &num3, &result_ptr2)?;
-        let result_ptr4 = AllocatedPtr::construct_cons(cs.namespace(|| "limb_4"), g, store, &num4, &result_ptr3)?;
+        for (i, num) in enumerate(nums) {
+            result_ptr = AllocatedPtr::construct_cons(cs.namespace(|| format!("limb_{i}")), g, store, &num, &result_ptr.clone())?
+        }
 
-        Ok((result_ptr4, input_env.clone(), input_cont.clone()))
+        Ok((result_ptr, input_env.clone(), input_cont.clone()))
     }
 }
 
@@ -176,16 +174,17 @@ fn main() {
 
     let coproc_expr = format!("({})", sym_str);
 
-    let u1: u64 = u64::from_be_bytes(expect[0..8].try_into().unwrap());
-    let u2: u64 = u64::from_be_bytes(expect[8..16].try_into().unwrap());
-    let u3: u64 = u64::from_be_bytes(expect[16..24].try_into().unwrap());
-    let u4: u64 = u64::from_be_bytes(expect[24..32].try_into().unwrap());
-    let result_expr = format!("({}u64 {}u64 {}u64 {}u64)", u1, u2, u3, u4);
+    let mut u: [u64;4] = [0u64; 4];
+    
+    for i in 0..4 {
+        u[i] = u64::from_be_bytes(expect[(i * 8) .. (i + 1) * 8].try_into().unwrap())
+    }
+    let result_expr = format!("({}u64 {}u64 {}u64 {}u64)", u[0], u[1], u[2], u[3]);
 
     let expr = format!("(emit (eq {coproc_expr} (quote {result_expr})))");
     let ptr = store.read(&expr).unwrap();
     
-    let reduction_count = 10;
+    let reduction_count = 1;
     
     let nova_prover = NovaProver::<Fr, Sha256Coproc<Fr>>::new(reduction_count, lang.clone());
 
