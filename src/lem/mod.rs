cc838c706502a5510888a06e0ad848d205e1ac99
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-05-04 19:30:38 -0300

    more structure, progress on interpretation, some documentation

--- src/lem/mod.rs
+++ src/lem/mod.rs
@@ -1,4 +1,5 @@
 mod ptr;
+mod step;
 mod store;
 mod tag;
 
@@ -19,83 +20,102 @@ impl<'a> MetaPtr<'a> {
 }
 
 #[derive(Clone)]
-pub enum OP<'a, F: LurkField> {
-    Set(MetaPtr<'a>, F, F),
-    Copy(MetaPtr<'a>, MetaPtr<'a>),
-    HashFPtr(F, MetaPtr<'a>, F, MetaPtr<'a>),
-    Hash2Ptrs(F, MetaPtr<'a>, [MetaPtr<'a>; 2]),
-    Hash3Ptrs(F, MetaPtr<'a>, [MetaPtr<'a>; 3]),
-    Hash4Ptrs(F, MetaPtr<'a>, [MetaPtr<'a>; 4]),
+pub enum LEMOP<'a, F: LurkField> {
+    New(MetaPtr<'a>, F, F),                        // tgt, tag, val
+    NewFrom(MetaPtr<'a>, MetaPtr<'a>),             // tgt, src
+    HashFPtr(MetaPtr<'a>, F, F, MetaPtr<'a>),      // tgt, tag, secret, src
+    Hash2Ptrs(MetaPtr<'a>, F, [MetaPtr<'a>; 2]),   // tgt, tag, srcs
+    Hash3Ptrs(MetaPtr<'a>, F, [MetaPtr<'a>; 3]),   // tgt, tag, srcs
+    Hash4Ptrs(MetaPtr<'a>, F, [MetaPtr<'a>; 4]),   // tgt, tag, srcs
     CarCdr(MetaPtr<'a>, MetaPtr<'a>, MetaPtr<'a>), // car, cdr, cons src
-    MatchTag(MetaPtr<'a>, BTreeMap<F, OP<'a, F>>, Box<OP<'a, F>>),
-    Err(&'a str),
-    Seq(Vec<OP<'a, F>>),
+    MatchTag(MetaPtr<'a>, BTreeMap<F, LEMOP<'a, F>>, Box<LEMOP<'a, F>>), // src, cases, default
+    Err(&'a str),                                  // throw an error
+    Seq(Vec<LEMOP<'a, F>>),                        // a sequence of operations
 }
 
-pub struct LEM<'a, F: LurkField> {
-    input: [&'a str; 3],
-    output: [&'a str; 3],
-    op: OP<'a, F>,
-}
-
-impl<'a, F: LurkField + std::cmp::Ord + std::hash::Hash> LEM<'a, F> {
-    pub fn assert_tag_eq(ptr: MetaPtr<'a>, val: F, ff: OP<'a, F>, tt: OP<'a, F>) -> OP<'a, F> {
+impl<'a, F: LurkField + std::cmp::Ord> LEMOP<'a, F> {
+    pub fn assert_tag_eq(
+        ptr: MetaPtr<'a>,
+        val: F,
+        ff: LEMOP<'a, F>,
+        tt: LEMOP<'a, F>,
+    ) -> LEMOP<'a, F> {
         let mut map = BTreeMap::new();
         map.insert(val, tt);
-        OP::MatchTag(ptr, map, Box::new(ff))
+        LEMOP::MatchTag(ptr, map, Box::new(ff))
     }
 
     pub fn assert_tag_or(
         ptr: MetaPtr<'a>,
         val1: F,
         val2: F,
-        ff: OP<'a, F>,
-        tt: OP<'a, F>,
-    ) -> OP<'a, F> {
+        ff: LEMOP<'a, F>,
+        tt: LEMOP<'a, F>,
+    ) -> LEMOP<'a, F> {
         let mut map = BTreeMap::new();
         map.insert(val1, tt.clone());
         map.insert(val2, tt);
-        OP::MatchTag(ptr, map, Box::new(ff))
+        LEMOP::MatchTag(ptr, map, Box::new(ff))
     }
 
-    pub fn assert_list(ptr: MetaPtr<'a>, ff: OP<'a, F>, tt: OP<'a, F>) -> OP<'a, F> {
+    pub fn assert_list(ptr: MetaPtr<'a>, ff: LEMOP<'a, F>, tt: LEMOP<'a, F>) -> LEMOP<'a, F> {
         Self::assert_tag_or(ptr, Tag::Cons.to_field(), Tag::Nil.to_field(), ff, tt)
     }
 
-    pub fn mk_comm(o: &'a str, s: F, i: MetaPtr<'a>) -> OP<'a, F> {
-        OP::HashFPtr(Tag::Comm.to_field(), MetaPtr(o), s, i)
+    pub fn mk_comm(o: &'a str, s: F, i: MetaPtr<'a>) -> LEMOP<'a, F> {
+        LEMOP::HashFPtr(MetaPtr(o), Tag::Comm.to_field(), s, i)
     }
 
-    pub fn mk_cons(o: &'a str, i: [MetaPtr<'a>; 2]) -> OP<'a, F> {
-        OP::Hash2Ptrs(Tag::Cons.to_field(), MetaPtr(o), i)
+    pub fn mk_cons(o: &'a str, i: [MetaPtr<'a>; 2]) -> LEMOP<'a, F> {
+        LEMOP::Hash2Ptrs(MetaPtr(o), Tag::Cons.to_field(), i)
     }
 
-    pub fn mk_strcons(o: &'a str, i: [MetaPtr<'a>; 2]) -> OP<'a, F> {
+    pub fn mk_strcons(o: &'a str, i: [MetaPtr<'a>; 2]) -> LEMOP<'a, F> {
         Self::assert_tag_eq(
             i[0],
             Tag::Char.to_field(),
-            OP::Err("strcons requires a char as the first argument"),
+            LEMOP::Err("strcons requires a char as the first argument"),
             Self::assert_tag_eq(
                 i[1],
                 Tag::Str.to_field(),
-                OP::Err("strcons requires a str as the second argument"),
-                OP::Hash2Ptrs(Tag::Str.to_field(), MetaPtr(o), i),
+                LEMOP::Err("strcons requires a str as the second argument"),
+                LEMOP::Hash2Ptrs(MetaPtr(o), Tag::Str.to_field(), i),
             ),
         )
     }
 
-    pub fn mk_fun(o: &'a str, i: [MetaPtr<'a>; 3]) -> OP<'a, F> {
+    pub fn mk_fun(o: &'a str, i: [MetaPtr<'a>; 3]) -> LEMOP<'a, F> {
         Self::assert_list(
             i[0],
-            OP::Err("The arguments must be a list"),
+            LEMOP::Err("The arguments must be a list"),
             Self::assert_list(
                 i[2],
-                OP::Err("The closed env must be a list"),
-                OP::Hash3Ptrs(Tag::Fun.to_field(), MetaPtr(o), i),
+                LEMOP::Err("The closed env must be a list"),
+                LEMOP::Hash3Ptrs(MetaPtr(o), Tag::Fun.to_field(), i),
             ),
         )
     }
 
+    pub fn mk_match_tag(
+        i: MetaPtr<'a>,
+        cases: Vec<(F, LEMOP<'a, F>)>,
+        def: LEMOP<'a, F>,
+    ) -> LEMOP<'a, F> {
+        let mut match_map = BTreeMap::default();
+        for (f, op) in cases.iter() {
+            match_map.insert(*f, op.clone());
+        }
+        LEMOP::MatchTag(i, match_map, Box::new(def))
+    }
+}
+
+pub struct LEM<'a, F: LurkField> {
+    input: [&'a str; 3],
+    output: [&'a str; 3],
+    lem_op: LEMOP<'a, F>,
+}
+
+impl<'a, F: LurkField + std::cmp::Ord + std::hash::Hash> LEM<'a, F> {
     pub fn check(&self) {
         for s in self.input.iter() {
             assert!(
@@ -111,46 +131,56 @@ impl<'a, F: LurkField + std::cmp::Ord + std::hash::Hash> LEM<'a, F> {
         // TODO: assert that all output pointers are defined
     }
 
-    pub fn run(&self, i: [Ptr; 3], map: &mut HashMap<&'a str, Ptr>, store: &mut Store<F>) {
+    // pub fn compile should generate the circuit
+
+    pub fn run(&self, i: [Ptr; 3], store: &mut Store<F>) -> Result<[Ptr; 3], String> {
+        // key/val pairs on this map should never be overwritten
+        let mut map: HashMap<&'a str, Ptr> = HashMap::default();
         map.insert(self.input[0], i[0]);
         map.insert(self.input[1], i[1]);
         map.insert(self.input[2], i[2]);
-        let mut stack: Vec<&OP<'a, F>> = vec![&self.op];
+        let mut stack = vec![&self.lem_op];
         while let Some(op) = stack.pop() {
             match op {
-                OP::HashFPtr(tag, dst, sct, src) => {
+                LEMOP::HashFPtr(tgt, tag, sct, src) => {
                     let src_ptr = map.get(src.name()).unwrap();
                     let (idx, _) = store.f_ptr_store.insert_full((*sct, *src_ptr));
-                    let dst_ptr = Ptr {
+                    let tgt_ptr = Ptr {
                         tag: Tag::from_field(*tag).unwrap(),
                         idx,
                     };
-                    map.insert(dst.name(), dst_ptr);
+                    if map.insert(tgt.name(), tgt_ptr).is_some() {
+                        return Err(format!("{} already defined. Malformed LEM", tgt.name()));
+                    }
                 }
-                OP::Hash2Ptrs(tag, dst, src) => {
+                LEMOP::Hash2Ptrs(tgt, tag, src) => {
                     let src_ptr1 = map.get(src[0].name()).unwrap();
                     let src_ptr2 = map.get(src[1].name()).unwrap();
                     let (idx, _) = store.ptr2_store.insert_full((*src_ptr1, *src_ptr2));
-                    let dst_ptr = Ptr {
+                    let tgt_ptr = Ptr {
                         tag: Tag::from_field(*tag).unwrap(),
                         idx,
                     };
-                    map.insert(dst.name(), dst_ptr);
+                    if map.insert(tgt.name(), tgt_ptr).is_some() {
+                        return Err(format!("{} already defined. Malformed LEM", tgt.name()));
+                    }
                 }
-                OP::Hash3Ptrs(tag, dst, src) => {
+                LEMOP::Hash3Ptrs(tgt, tag, src) => {
                     let src_ptr1 = map.get(src[0].name()).unwrap();
                     let src_ptr2 = map.get(src[1].name()).unwrap();
                     let src_ptr3 = map.get(src[2].name()).unwrap();
                     let (idx, _) = store
                         .ptr3_store
                         .insert_full((*src_ptr1, *src_ptr2, *src_ptr3));
-                    let dst_ptr = Ptr {
+                    let tgt_ptr = Ptr {
                         tag: Tag::from_field(*tag).unwrap(),
                         idx,
                     };
-                    map.insert(dst.name(), dst_ptr);
+                    if map.insert(tgt.name(), tgt_ptr).is_some() {
+                        return Err(format!("{} already defined. Malformed LEM", tgt.name()));
+                    }
                 }
-                OP::Hash4Ptrs(tag, dst, src) => {
+                LEMOP::Hash4Ptrs(tgt, tag, src) => {
                     let src_ptr1 = map.get(src[0].name()).unwrap();
                     let src_ptr2 = map.get(src[1].name()).unwrap();
                     let src_ptr3 = map.get(src[2].name()).unwrap();
@@ -158,48 +188,23 @@ impl<'a, F: LurkField + std::cmp::Ord + std::hash::Hash> LEM<'a, F> {
                     let (idx, _) = store
                         .ptr4_store
                         .insert_full((*src_ptr1, *src_ptr2, *src_ptr3, *src_ptr4));
-                    let dst_ptr = Ptr {
+                    let tgt_ptr = Ptr {
                         tag: Tag::from_field(*tag).unwrap(),
                         idx,
                     };
-                    map.insert(dst.name(), dst_ptr);
+                    if map.insert(tgt.name(), tgt_ptr).is_some() {
+                        return Err(format!("{} already defined. Malformed LEM", tgt.name()));
+                    }
                 }
-                OP::Seq(ops) => stack.extend(ops.iter().rev()),
+                LEMOP::Seq(ops) => stack.extend(ops.iter().rev()),
+                LEMOP::Err(s) => return Err(s.to_string()), // this should use the error continuation
                 _ => todo!(),
             }
         }
+        Ok([
+            *map.get(self.output[0]).unwrap(),
+            *map.get(self.output[1]).unwrap(),
+            *map.get(self.output[2]).unwrap(),
+        ])
     }
-}
-
-pub fn step<'a, F: LurkField + std::cmp::Ord>() -> LEM<'a, F> {
-    let input = ["expr_in", "env_in", "cont_in"];
-    let output = ["expr_out", "env_out", "cont_out"];
-    let op = OP::MatchTag(
-        MetaPtr("expr_in"),
-        {
-            let mut match_map = BTreeMap::default();
-            match_map.insert(
-                Tag::Num.to_field(),
-                OP::MatchTag(
-                    MetaPtr("cont_in"),
-                    {
-                        let mut match_map = BTreeMap::default();
-                        match_map.insert(
-                            Tag::Outermost.to_field(),
-                            OP::Seq(vec![
-                                OP::Copy(MetaPtr("expr_out"), MetaPtr("expr_in")),
-                                OP::Copy(MetaPtr("env_out"), MetaPtr("env_in")),
-                                OP::Set(MetaPtr("cont_out"), Tag::Terminal.to_field(), F::zero()),
-                            ]),
-                        );
-                        match_map
-                    },
-                    Box::new(OP::Err("todo")),
-                ),
-            );
-            match_map
-        },
-        Box::new(OP::Err("todo")),
-    );
-    LEM { input, output, op }
 }
---
+++ src/lem/step.rs
@@ -0,0 +1,32 @@
+use crate::field::LurkField;
+
+use super::{tag::Tag, MetaPtr, LEM, LEMOP};
+
+pub fn step<'a, F: LurkField + std::cmp::Ord>() -> LEM<'a, F> {
+    let input = ["expr_in", "env_in", "cont_in"];
+    let output = ["expr_out", "env_out", "cont_out"];
+    let lem_op = LEMOP::mk_match_tag(
+        MetaPtr("expr_in"),
+        vec![(
+            Tag::Num.to_field(),
+            LEMOP::mk_match_tag(
+                MetaPtr("cont_in"),
+                vec![(
+                    Tag::Outermost.to_field(),
+                    LEMOP::Seq(vec![
+                        LEMOP::NewFrom(MetaPtr("expr_out"), MetaPtr("expr_in")),
+                        LEMOP::NewFrom(MetaPtr("env_out"), MetaPtr("env_in")),
+                        LEMOP::New(MetaPtr("cont_out"), Tag::Terminal.to_field(), F::zero()),
+                    ]),
+                )],
+                LEMOP::Err("Invalid continuation tag"),
+            ),
+        )],
+        LEMOP::Err("Invalid expression tag"),
+    );
+    LEM {
+        input,
+        output,
+        lem_op,
+    }
+}
