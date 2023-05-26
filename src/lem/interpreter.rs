190a25cc6cd63dfc04373b2072630d6348e85f53
François Garillot <francois@garillot.net>
2023-05-26 10:51:47 -0400

    refactor: Refactor hashmap insertions in interpreter.rs
    - Implement `insert_into_ptrs` function to streamline hashmap insertion and error handling
    - Refactor existing hashmap insertions to use the new function for consistency and simplicity

--- src/lem/interpreter.rs
+++ src/lem/interpreter.rs
@@ -4,6 +4,17 @@ use std::collections::HashMap;
 
 use super::{pointers::Ptr, store::Store, symbol::Symbol, tag::Tag, Witness, LEM, LEMOP};
 
+fn insert_into_ptrs<F: LurkField>(
+    ptrs: &mut HashMap<String, Ptr<F>>,
+    key: String,
+    value: Ptr<F>,
+) -> Result<()> {
+    if ptrs.insert(key.clone(), value).is_some() {
+        bail!("{} already defined", key);
+    }
+    Ok(())
+}
+
 impl LEM {
     /// Interprets a LEM using a stack of operations to be popped and executed.
     /// It modifies a `Store` and assigns `Ptr`s to `MetaPtr`s as it goes.
@@ -15,38 +26,28 @@ impl LEM {
         // key/val pairs on this map should never be overwritten
         let mut ptrs = HashMap::default();
         ptrs.insert(self.input[0].clone(), input[0]);
-        if ptrs.insert(self.input[1].clone(), input[1]).is_some() {
-            bail!("{} already defined", self.input[1]);
-        }
-        if ptrs.insert(self.input[2].clone(), input[2]).is_some() {
-            bail!("{} already defined", self.input[2]);
-        }
+        insert_into_ptrs(&mut ptrs, self.input[1].clone(), input[1])?;
+        insert_into_ptrs(&mut ptrs, self.input[2].clone(), input[2])?;
         let mut output = None;
         let mut stack = vec![&self.lem_op];
         while let Some(op) = stack.pop() {
             match op {
                 LEMOP::MkNull(tgt, tag) => {
                     let tgt_ptr = Ptr::null(*tag);
-                    if ptrs.insert(tgt.name().clone(), tgt_ptr).is_some() {
-                        bail!("{} already defined", tgt.name());
-                    }
+                    insert_into_ptrs(&mut ptrs, tgt.name().clone(), tgt_ptr)?;
                 }
                 LEMOP::Hash2Ptrs(tgt, tag, src) => {
                     let src_ptr1 = src[0].get_ptr(&ptrs)?;
                     let src_ptr2 = src[1].get_ptr(&ptrs)?;
                     let tgt_ptr = store.intern_2_ptrs(*tag, src_ptr1, src_ptr2);
-                    if ptrs.insert(tgt.name().clone(), tgt_ptr).is_some() {
-                        bail!("{} already defined", tgt.name());
-                    }
+                    insert_into_ptrs(&mut ptrs, tgt.name().clone(), tgt_ptr)?;
                 }
                 LEMOP::Hash3Ptrs(tgt, tag, src) => {
                     let src_ptr1 = src[0].get_ptr(&ptrs)?;
                     let src_ptr2 = src[1].get_ptr(&ptrs)?;
                     let src_ptr3 = src[2].get_ptr(&ptrs)?;
                     let tgt_ptr = store.intern_3_ptrs(*tag, src_ptr1, src_ptr2, src_ptr3);
-                    if ptrs.insert(tgt.name().clone(), tgt_ptr).is_some() {
-                        bail!("{} already defined", tgt.name());
-                    }
+                    insert_into_ptrs(&mut ptrs, tgt.name().clone(), tgt_ptr)?;
                 }
                 LEMOP::Hash4Ptrs(tgt, tag, src) => {
                     let src_ptr1 = src[0].get_ptr(&ptrs)?;
@@ -54,9 +55,7 @@ impl LEM {
                     let src_ptr3 = src[2].get_ptr(&ptrs)?;
                     let src_ptr4 = src[3].get_ptr(&ptrs)?;
                     let tgt_ptr = store.intern_4_ptrs(*tag, src_ptr1, src_ptr2, src_ptr3, src_ptr4);
-                    if ptrs.insert(tgt.name().clone(), tgt_ptr).is_some() {
-                        bail!("{} already defined", tgt.name());
-                    }
+                    insert_into_ptrs(&mut ptrs, tgt.name().clone(), tgt_ptr)?;
                 }
                 LEMOP::Unhash2Ptrs(tgts, src) => {
                     let src_ptr = src.get_ptr(&ptrs)?;
@@ -69,12 +68,8 @@ impl LEM {
                     let Some((a, b)) = store.fetch_2_ptrs(idx) else {
                         bail!("Couldn't fetch {}'s children", src.name())
                     };
-                    if ptrs.insert(tgts[0].name().clone(), *a).is_some() {
-                        bail!("{} already defined", tgts[0].name());
-                    }
-                    if ptrs.insert(tgts[1].name().clone(), *b).is_some() {
-                        bail!("{} already defined", tgts[1].name());
-                    }
+                    insert_into_ptrs(&mut ptrs, tgts[0].name().clone(), *a)?;
+                    insert_into_ptrs(&mut ptrs, tgts[1].name().clone(), *b)?;
                 }
                 LEMOP::Unhash3Ptrs(tgts, src) => {
                     let src_ptr = src.get_ptr(&ptrs)?;
@@ -87,15 +82,9 @@ impl LEM {
                     let Some((a, b, c)) = store.fetch_3_ptrs(idx) else {
                         bail!("Couldn't fetch {}'s children", src.name())
                     };
-                    if ptrs.insert(tgts[0].name().clone(), *a).is_some() {
-                        bail!("{} already defined", tgts[0].name());
-                    }
-                    if ptrs.insert(tgts[1].name().clone(), *b).is_some() {
-                        bail!("{} already defined", tgts[1].name());
-                    }
-                    if ptrs.insert(tgts[2].name().clone(), *c).is_some() {
-                        bail!("{} already defined", tgts[2].name());
-                    }
+                    insert_into_ptrs(&mut ptrs, tgts[0].name().clone(), *a)?;
+                    insert_into_ptrs(&mut ptrs, tgts[1].name().clone(), *b)?;
+                    insert_into_ptrs(&mut ptrs, tgts[2].name().clone(), *c)?;
                 }
                 LEMOP::Unhash4Ptrs(tgts, src) => {
                     let src_ptr = src.get_ptr(&ptrs)?;
@@ -108,18 +97,10 @@ impl LEM {
                     let Some((a, b, c, d)) = store.fetch_4_ptrs(idx) else {
                         bail!("Couldn't fetch {}'s children", src.name())
                     };
-                    if ptrs.insert(tgts[0].name().clone(), *a).is_some() {
-                        bail!("{} already defined", tgts[0].name());
-                    }
-                    if ptrs.insert(tgts[1].name().clone(), *b).is_some() {
-                        bail!("{} already defined", tgts[1].name());
-                    }
-                    if ptrs.insert(tgts[2].name().clone(), *c).is_some() {
-                        bail!("{} already defined", tgts[2].name());
-                    }
-                    if ptrs.insert(tgts[3].name().clone(), *d).is_some() {
-                        bail!("{} already defined", tgts[3].name());
-                    }
+                    insert_into_ptrs(&mut ptrs, tgts[0].name().clone(), *a)?;
+                    insert_into_ptrs(&mut ptrs, tgts[1].name().clone(), *b)?;
+                    insert_into_ptrs(&mut ptrs, tgts[2].name().clone(), *c)?;
+                    insert_into_ptrs(&mut ptrs, tgts[3].name().clone(), *d)?;
                 }
                 LEMOP::Hide(tgt, sec, src) => {
                     let src_ptr = src.get_ptr(&ptrs)?;
@@ -133,9 +114,7 @@ impl LEM {
                             .hash3(&[secret, z_ptr.tag.to_field(), z_ptr.hash]);
                     let tgt_ptr = Ptr::comm(hash);
                     store.comms.insert(FWrap::<F>(hash), (secret, src_ptr));
-                    if ptrs.insert(tgt.name().clone(), tgt_ptr).is_some() {
-                        bail!("{} already defined", tgt.name());
-                    }
+                    insert_into_ptrs(&mut ptrs, tgt.name().clone(), tgt_ptr)?;
                 }
                 LEMOP::Open(tgt_secret, tgt_ptr, comm_or_num) => {
                     match comm_or_num.get_ptr(&ptrs)? {
@@ -143,15 +122,12 @@ impl LEM {
                             let Some((secret, ptr)) = store.comms.get(&FWrap::<F>(hash)) else {
                                 bail!("No committed data for hash {}", &hash.hex_digits())
                             };
-                            if ptrs.insert(tgt_ptr.name().clone(), *ptr).is_some() {
-                                bail!("{} already defined", tgt_ptr.name());
-                            }
-                            if ptrs
-                                .insert(tgt_secret.name().clone(), Ptr::Leaf(Tag::Num, *secret))
-                                .is_some()
-                            {
-                                bail!("{} already defined", tgt_secret.name());
-                            }
+                            insert_into_ptrs(&mut ptrs, tgt_ptr.name().clone(), *ptr)?;
+                            insert_into_ptrs(
+                                &mut ptrs,
+                                tgt_secret.name().clone(),
+                                Ptr::Leaf(Tag::Num, *secret),
+                            )?;
                         }
                         _ => {
                             bail!("{} is not a num/comm pointer", comm_or_num.name())
