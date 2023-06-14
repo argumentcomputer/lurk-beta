eecac95e84155d264b3b6c423f35aa57ea0a55e9
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2023-06-14 13:38:25 -0300

    Fixed number of constraints

--- src/circuit/circuit_frame.rs
+++ src/circuit/circuit_frame.rs
@@ -5308,9 +5308,9 @@ mod tests {
             assert!(delta == Delta::Equal);
 
             //println!("{}", print_cs(&cs));
-            assert_eq!(12035, cs.num_constraints());
+            assert_eq!(12032, cs.num_constraints());
             assert_eq!(13, cs.num_inputs());
-            assert_eq!(11690, cs.aux().len());
+            assert_eq!(11688, cs.aux().len());
 
             let public_inputs = multiframe.public_inputs();
             let mut rng = rand::thread_rng();
