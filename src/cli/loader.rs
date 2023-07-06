f5ceecb0b528f80a898b614019c056d27f676c59
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-07-06 19:35:26 -0300

    more plumbing

--- src/cli/loader.rs
+++ src/cli/loader.rs
@@ -81,38 +81,60 @@ impl<F: LurkField + serde::Serialize + for<'de> serde::Deserialize<'de>, C: Copr
         })
     }
 
+    pub fn prove_last_claim(&mut self) -> Result<()> {
+        match &self.last_claim {
+            Some(claim) => {
+                // TODO
+                let _proof = prove_claim(claim);
+                Ok(())
+            }
+            None => {
+                bail!("No claim to prove");
+            }
+        }
+    }
+
     #[inline]
     fn eval_expr(&mut self, expr_ptr: Ptr<F>) -> Result<(IO<F>, usize, Vec<Ptr<F>>)> {
         Ok(Evaluator::new(expr_ptr, self.env, &mut self.store, self.limit, &self.lang).eval()?)
     }
 
+    fn peek1(&self, cmd: &str, args: &Ptr<F>) -> Result<Ptr<F>> {
+        let (first, rest) = self.store.car_cdr(args)?;
+        if !rest.is_nil() {
+            bail!("`{cmd}` accepts at most one argument")
+        }
+        Ok(first)
+    }
+
+    fn peek2(&self, cmd: &str, args: &Ptr<F>) -> Result<(Ptr<F>, Ptr<F>)> {
+        let (first, rest) = self.store.car_cdr(args)?;
+        let (second, rest) = self.store.car_cdr(&rest)?;
+        if !rest.is_nil() {
+            bail!("`{cmd}` accepts at most two arguments")
+        }
+        Ok((first, second))
+    }
+
     fn handle_meta_cases(&mut self, cmd: &str, args: &Ptr<F>, pwd_path: &Path) -> Result<()> {
         match cmd {
             "assert" => {
-                let (first, rest) = &self.store.car_cdr(args)?;
-                if !rest.is_nil() {
-                    bail!("`assert` accepts at most one argument")
-                }
-                let (first_io, ..) = self.eval_expr(*first)?;
+                let first = self.peek1(cmd, args)?;
+                let (first_io, ..) = self.eval_expr(first)?;
                 if first_io.expr.is_nil() {
                     bail!(
                         "`assert` failed. {} evaluates to `nil`",
                         first.fmt_to_string(&self.store)
                     )
                 }
-                Ok(())
             }
             "assert-eq" => {
-                let (first, rest) = &self.store.car_cdr(args)?;
-                let (second, rest) = &self.store.car_cdr(rest)?;
-                if !rest.is_nil() {
-                    bail!("`assert-eq` accepts at most two arguments")
-                }
+                let (first, second) = self.peek2(cmd, args)?;
                 let (first_io, ..) = self
-                    .eval_expr(*first)
+                    .eval_expr(first)
                     .with_context(|| "evaluating first arg")?;
                 let (second_io, ..) = self
-                    .eval_expr(*second)
+                    .eval_expr(second)
                     .with_context(|| "evaluating second arg")?;
                 if !&self.store.ptr_eq(&first_io.expr, &second_io.expr)? {
                     bail!(
@@ -123,22 +145,16 @@ impl<F: LurkField + serde::Serialize + for<'de> serde::Deserialize<'de>, C: Copr
                         second_io.expr.fmt_to_string(&self.store)
                     )
                 }
-                Ok(())
             }
             "assert-emitted" => {
-                let (first, rest) = &self.store.car_cdr(args)?;
-                let (second, rest) = &self.store.car_cdr(rest)?;
-
-                if !rest.is_nil() {
-                    bail!("`assert-emitted` accepts at most two arguments")
-                }
+                let (first, second) = self.peek2(cmd, args)?;
                 let (first_io, ..) = self
-                    .eval_expr(*first)
+                    .eval_expr(first)
                     .with_context(|| "evaluating first arg")?;
                 let (.., emitted) = self
-                    .eval_expr(*second)
+                    .eval_expr(second)
                     .with_context(|| "evaluating second arg")?;
-                let (mut first_emitted, mut rest_emitted) = &self.store.car_cdr(&first_io.expr)?;
+                let (mut first_emitted, mut rest_emitted) = self.store.car_cdr(&first_io.expr)?;
                 for (i, elem) in emitted.iter().enumerate() {
                     if elem != &first_emitted {
                         bail!(
@@ -149,26 +165,18 @@ impl<F: LurkField + serde::Serialize + for<'de> serde::Deserialize<'de>, C: Copr
                     }
                     (first_emitted, rest_emitted) = self.store.car_cdr(&rest_emitted)?;
                 }
-                Ok(())
             }
             "assert-error" => {
-                let (first, rest) = &self.store.car_cdr(args)?;
-                if !rest.is_nil() {
-                    bail!("`assert-error` accepts at most one argument")
-                }
-                let (first_io, ..) = self.eval_expr(*first)?;
+                let first = self.peek1(cmd, args)?;
+                let (first_io, ..) = self.eval_expr(first)?;
                 if first_io.cont.tag != ContTag::Error {
                     bail!(
                         "`assert-error` failed. {} doesn't result on evaluation error.",
                         first.fmt_to_string(&self.store)
                     )
                 }
-                Ok(())
             }
-            "clear" => {
-                self.env = self.store.nil();
-                Ok(())
-            }
+            "clear" => self.env = self.store.nil(),
             "def" => {
                 // Extends env with a non-recursive binding.
                 //
@@ -178,14 +186,10 @@ impl<F: LurkField + serde::Serialize + for<'de> serde::Deserialize<'de>, C: Copr
                 //                               (current-env))
                 //
                 // And the state's env is set to the result.
-                let (first, rest) = &self.store.car_cdr(args)?;
-                let (second, rest) = &self.store.car_cdr(rest)?;
-                if !rest.is_nil() {
-                    bail!("`def` accepts at most two arguments")
-                }
+                let (first, second) = self.peek2(cmd, args)?;
                 let l = &self.store.lurk_sym("let");
                 let current_env = &self.store.lurk_sym("current-env");
-                let binding = &self.store.list(&[*first, *second]);
+                let binding = &self.store.list(&[first, second]);
                 let bindings = &self.store.list(&[*binding]);
                 let current_env_call = &self.store.list(&[*current_env]);
                 let expanded = &self.store.list(&[*l, *bindings, *current_env_call]);
@@ -196,7 +200,6 @@ impl<F: LurkField + serde::Serialize + for<'de> serde::Deserialize<'de>, C: Copr
                 let (new_binding, _) = &self.store.car_cdr(&expanded_io.expr)?;
                 let (new_name, _) = self.store.car_cdr(new_binding)?;
                 println!("{}", new_name.fmt_to_string(&self.store));
-                Ok(())
             }
             "defrec" => {
                 // Extends env with a recursive binding.
@@ -207,14 +210,10 @@ impl<F: LurkField + serde::Serialize + for<'de> serde::Deserialize<'de>, C: Copr
                 //                               (current-env))
                 //
                 // And the state's env is set to the result.
-                let (first, rest) = &self.store.car_cdr(args)?;
-                let (second, rest) = &self.store.car_cdr(rest)?;
-                if !rest.is_nil() {
-                    bail!("`defrec` accepts at most two arguments")
-                }
+                let (first, second) = self.peek2(cmd, args)?;
                 let l = &self.store.lurk_sym("letrec");
                 let current_env = &self.store.lurk_sym("current-env");
-                let binding = &self.store.list(&[*first, *second]);
+                let binding = &self.store.list(&[first, second]);
                 let bindings = &self.store.list(&[*binding]);
                 let current_env_call = &self.store.list(&[*current_env]);
                 let expanded = &self.store.list(&[*l, *bindings, *current_env_call]);
@@ -226,48 +225,35 @@ impl<F: LurkField + serde::Serialize + for<'de> serde::Deserialize<'de>, C: Copr
                 let (new_binding_inner, _) = &self.store.car_cdr(new_binding_outer)?;
                 let (new_name, _) = self.store.car_cdr(new_binding_inner)?;
                 println!("{}", new_name.fmt_to_string(&self.store));
-                Ok(())
             }
             "load" => {
-                let (first, rest) = &self.store.car_cdr(args)?;
-                if !rest.is_nil() {
-                    bail!("`load` accepts at most one argument")
-                }
-                match &self.store.fetch(first).unwrap() {
-                    Expression::Str(..) => {
-                        let path = &self.store.fetch_string(first).unwrap();
+                let first = self.peek1(cmd, args)?;
+                match self.store.fetch_string(&first) {
+                    Some(path) => {
                         let joined = pwd_path.join(Path::new(&path));
                         self.load_file(&joined)?
                     }
                     _ => bail!("Argument of `load` must be a string."),
                 }
                 io::Write::flush(&mut io::stdout()).unwrap();
-                Ok(())
             }
             "set-env" => {
                 // The state's env is set to the result of evaluating the first argument.
-                let (first, rest) = &self.store.car_cdr(args)?;
-                if !rest.is_nil() {
-                    bail!("`set-env` accepts at most one argument")
-                }
-                let (first_io, ..) = self.eval_expr(*first)?;
+                let first = self.peek1(cmd, args)?;
+                let (first_io, ..) = self.eval_expr(first)?;
                 self.env = first_io.expr;
-                Ok(())
             }
             "set-rc" => {
-                let (first, rest) = &self.store.car_cdr(args)?;
-                if !rest.is_nil() {
-                    bail!("`set-rc` accepts at most one argument")
-                }
+                let first = self.peek1(cmd, args)?;
                 let rc = match first.tag {
-                    ExprTag::Num => match self.store.fetch_num(first).unwrap() {
+                    ExprTag::Num => match self.store.fetch_num(&first).unwrap() {
                         Num::U64(u) => *u as usize,
                         _ => bail!(
                             "Invalid value for `rc`: {}",
                             first.fmt_to_string(&self.store)
                         ),
                     },
-                    ExprTag::U64 => match self.store.fetch_uint(first).unwrap() {
+                    ExprTag::U64 => match self.store.fetch_uint(&first).unwrap() {
                         UInt::U64(u) => u as usize,
                     },
                     _ => bail!(
@@ -280,16 +266,19 @@ impl<F: LurkField + serde::Serialize + for<'de> serde::Deserialize<'de>, C: Copr
                 // TODO: improve this
                 warn!("Warning: changing `rc` resets the proof cache");
                 self.proof_map = nova_proof_cache(rc);
-                Ok(())
             }
             "prove" => {
-                todo!()
+                if !args.is_nil() {
+                    self.eval_expr_and_set_last_claim(self.peek1(cmd, args)?)?;
+                }
+                self.prove_last_claim()?;
             }
             "verify" => {
                 todo!()
             }
             _ => bail!("Unsupported meta command: {cmd}"),
         }
+        Ok(())
     }
 
     fn handle_meta(&mut self, expr_ptr: Ptr<F>, pwd_path: &Path) -> Result<()> {
@@ -440,17 +429,5 @@ impl<F: LurkField + serde::Serialize + for<'de> serde::Deserialize<'de>, C: Copr
             }
         }
         Ok(())
-    }
-
-    pub fn prove_last_claim(&mut self) -> Result<()> {
-        match &self.last_claim {
-            Some(claim) => {
-                let _ = prove_claim(claim);
-                Ok(())
-            }
-            None => {
-                bail!("No claim to prove");
-            }
-        }
     }
 }
