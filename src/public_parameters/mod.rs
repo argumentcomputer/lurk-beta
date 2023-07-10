f8fe98128172156794ab07355943dc7d6b5f307c
Matej Penciak <matej.penciak@gmail.com>
2023-07-10 14:50:20 -0400

    fix public_param impl block

--- src/public_parameters/mod.rs
+++ src/public_parameters/mod.rs
@@ -27,13 +27,14 @@ pub fn public_params<C: Coprocessor<S1> + Serialize + DeserializeOwned + 'static
     let f = |lang: Arc<Lang<S1, C>>| Arc::new(nova::public_params(rc, lang));
     registry::CACHE_REG.get_coprocessor_or_update_with(rc, f, lang)
 }
-
 pub trait FileStore
 where
     Self: Sized,
 {
     fn write_to_path<P: AsRef<Path>>(&self, path: P);
+    fn write_to_json_path<P: AsRef<Path>>(&self, path: P);
     fn read_from_path<P: AsRef<Path>>(path: P) -> Result<Self, Error>;
+    fn read_from_json_path<P: AsRef<Path>>(path: P) -> Result<Self, Error>;
     fn read_from_stdin() -> Result<Self, Error>;
 }
 
@@ -48,24 +49,25 @@ where
         bincode::serialize_into(writer, &self).expect("failed to write file");
     }
 
-    fn read_from_path<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
-        use std::io::Read;
-        // FIXME: due to an bug in bincode, we must first copy out
-        // all the bytes before passing to `bincode::deserialize`.
-        // See: https://github.com/bincode-org/bincode/issues/633
-        let mut file = File::open(path)?;
-        let mut bytes = Vec::new();
-        file.read_to_end(&mut bytes).unwrap();
-        bincode::deserialize(&bytes).map_err(|e| {
-            eprintln!("{}", e);
-            Error::CacheError(e.to_string())
-        })
+    fn write_to_json_path<P: AsRef<Path>>(&self, path: P) {
+        let file = File::create(path).expect("failed to create file");
+        let writer = BufWriter::new(&file);
+
+        serde_json::to_writer(writer, &self).expect("failed to write file");
     }
-    //    let file = File::open(path)?;
-    //    let reader = BufReader::new(file);
-    //    bincode::deserialize_from(reader)
-    //        .map_err(|e| Error::CacheError(format!("Cache deserialization error: {}", e)))
-    //}
+
+    fn read_from_path<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        bincode::deserialize_from(reader)
+            .map_err(|e| Error::CacheError(format!("Cache deserialization error: {}", e)))
+    }
+
+    fn read_from_json_path<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        Ok(serde_json::from_reader(reader)?)
+    }
 
     fn read_from_stdin() -> Result<Self, Error> {
         let reader = BufReader::new(io::stdin());
