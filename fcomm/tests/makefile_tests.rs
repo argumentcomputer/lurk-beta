8a6a942e3fb8fc14544455a628b0b6edf7f125c3
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-07-12 13:47:50 -0300

    fmt

--- fcomm/tests/makefile_tests.rs
+++ fcomm/tests/makefile_tests.rs
@@ -5,30 +5,30 @@ use std::process::Command;
 #[ignore]
 #[test]
 fn test_make_fcomm_examples() {
-   // Get the current working directory
-   let mut current_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    // Get the current working directory
+    let mut current_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 
-   // Find the examples directory
-   current_dir.push("examples");
-   let examples_dir = current_dir.as_path();
-   assert!(
-       examples_dir.exists() && examples_dir.is_dir(),
-       "Failed to find the fcomm examples directory"
-   );
+    // Find the examples directory
+    current_dir.push("examples");
+    let examples_dir = current_dir.as_path();
+    assert!(
+        examples_dir.exists() && examples_dir.is_dir(),
+        "Failed to find the fcomm examples directory"
+    );
 
-   // Run the make command in the examples directory
-   let cpus = num_cpus::get();
+    // Run the make command in the examples directory
+    let cpus = num_cpus::get();
 
-   let make_output = Command::new("make")
-       .current_dir(&examples_dir)
-       .arg(format!("-j{}", cpus))
-       .output()
-       .expect("Failed to run the make command, is make installed?");
+    let make_output = Command::new("make")
+        .current_dir(&examples_dir)
+        .arg(format!("-j{}", cpus))
+        .output()
+        .expect("Failed to run the make command, is make installed?");
 
-   // Check the exit status
-   assert!(
-       make_output.status.success(),
-       "Make command exited with an error: {}",
-       String::from_utf8_lossy(&make_output.stderr)
-   );
+    // Check the exit status
+    assert!(
+        make_output.status.success(),
+        "Make command exited with an error: {}",
+        String::from_utf8_lossy(&make_output.stderr)
+    );
 }
