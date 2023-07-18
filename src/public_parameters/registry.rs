5aa59f31037c785689eee375fa71d2505229db22
François Garillot <4142+huitseeker@users.noreply.github.com>
2023-07-18 16:37:02 -0400

    fix remove uneeded Serialization bounds on Coprocessors (#540)
    Added in #88
    
    @winston-h-zhang very rightly notices they are not needed in https://github.com/lurk-lab/lurk-rs/tree/circom-integration

--- src/public_parameters/registry.rs
+++ src/public_parameters/registry.rs
@@ -6,7 +6,6 @@ use std::{
 use log::info;
 use once_cell::sync::Lazy;
 use pasta_curves::pallas;
-use serde::{de::DeserializeOwned, Serialize};
 use tap::TapFallible;
 
 use crate::public_parameters::error::Error;
@@ -34,7 +33,7 @@ pub(crate) static CACHE_REG: Lazy<Registry> = Lazy::new(|| Registry {
 
 impl Registry {
     fn get_from_file_cache_or_update_with<
-        C: Coprocessor<S1> + Serialize + DeserializeOwned + 'static,
+        C: Coprocessor<S1> + 'static,
         F: FnOnce(Arc<Lang<S1, C>>) -> Arc<PublicParams<'static, C>>,
     >(
         &'static self,
@@ -66,7 +65,7 @@ impl Registry {
     /// Check if params for this Coproc are in registry, if so, return them.
     /// Otherwise, initialize with the passed in function.
     pub(crate) fn get_coprocessor_or_update_with<
-        C: Coprocessor<S1> + Serialize + DeserializeOwned + 'static,
+        C: Coprocessor<S1> + 'static,
         F: FnOnce(Arc<Lang<S1, C>>) -> Arc<PublicParams<'static, C>>,
     >(
         &'static self,
