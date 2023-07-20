1d9deaea4b38b90e39bcd947b87d86e4bc5395dd
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-07-20 07:44:55 -0300

    add tests for field_data

--- src/cli/field_data.rs
+++ src/cli/field_data.rs
@@ -6,26 +6,42 @@ use serde::{de::DeserializeOwned, Deserialize, Serialize};
 // on an arithmetic field in order to be properly deserialized.
 //
 // First, we serialize it to a vector of bytes. Then, we wrap the vector with a
-// struct that contains the field modulus, which, in turn, is serialized to a
-// vector of bytes.
+// struct that contains the field modulus, which, in turn, is serialized to the
+// final vector of bytes.
 //
-// When deserializing, we unwrap the first layer and double check the field
+// When deserializing, we unwrap the vector of bytes and double check the field
 // modulus for consistency. If everything goes well, we further unwrap the second
-// layer of bytes.
+// vector of bytes.
 
 pub(crate) trait HasFieldModulus {
     fn field_modulus() -> String;
 }
 
 #[allow(dead_code)]
-pub(crate) fn to_bytes<T: Serialize + HasFieldModulus>(t: T) -> Result<Vec<u8>> {
+pub(crate) fn ser<T: Serialize + HasFieldModulus>(t: T) -> Result<Vec<u8>> {
     Ok(bincode::serialize(&FieldData(t))?)
 }
 
 #[allow(dead_code)]
-pub(crate) fn from_bytes<T: DeserializeOwned + HasFieldModulus>(bytes: &[u8]) -> Result<T> {
+pub(crate) fn de<T: DeserializeOwned + HasFieldModulus>(bytes: &[u8]) -> Result<T> {
     let FieldData(data) = bincode::deserialize(bytes)?;
     Ok(data)
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub mod non_wasm {
+    use super::{de, ser, HasFieldModulus};
+    use anyhow::Result;
+    use serde::{de::DeserializeOwned, Serialize};
+    use std::path::PathBuf;
+
+    pub(crate) fn dump<T: Serialize + HasFieldModulus>(t: T, path: PathBuf) -> Result<()> {
+        Ok(std::fs::write(path, ser(t)?)?)
+    }
+
+    pub(crate) fn load<T: DeserializeOwned + HasFieldModulus>(path: PathBuf) -> Result<T> {
+        de(&std::fs::read(path)?)
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -65,18 +81,89 @@ impl<T: Serialize + HasFieldModulus> Serialize for FieldData<T> {
     }
 }
 
-#[cfg(not(target_arch = "wasm32"))]
-pub mod non_wasm {
-    use super::{from_bytes, to_bytes, HasFieldModulus};
-    use anyhow::Result;
-    use serde::{de::DeserializeOwned, Serialize};
-    use std::path::PathBuf;
+#[cfg(test)]
+mod tests {
+    use ff::Field;
+    use lurk::field::LurkField;
+    use pasta_curves::Fq;
+    use serde::{Deserialize, Serialize};
 
-    pub(crate) fn dump<T: Serialize + HasFieldModulus>(t: T, path: PathBuf) -> Result<()> {
-        Ok(std::fs::write(path, to_bytes(t)?)?)
+    use super::{de, ser, HasFieldModulus};
+
+    #[derive(PartialEq, Debug, Clone, Serialize, Deserialize)]
+    struct Struct<F: LurkField> {
+        str: String,
+        int: i32,
+        ff: F,
     }
 
-    pub(crate) fn load<T: DeserializeOwned + HasFieldModulus>(path: PathBuf) -> Result<T> {
-        from_bytes(&std::fs::read(path)?)
+    #[derive(PartialEq, Debug, Clone, Serialize, Deserialize)]
+    enum Enum1<F: LurkField> {
+        CaseStr(String),
+        CaseInt(i32),
+        CaseFF(F),
+    }
+
+    impl<F: LurkField> HasFieldModulus for Struct<F> {
+        fn field_modulus() -> String {
+            F::MODULUS.to_string()
+        }
+    }
+
+    impl<F: LurkField> HasFieldModulus for Enum1<F> {
+        fn field_modulus() -> String {
+            F::MODULUS.to_string()
+        }
+    }
+
+    #[test]
+    fn struct_roundtrips() {
+        let s = Struct {
+            str: "hi".into(),
+            int: 42,
+            ff: Fq::double(&Fq::ONE),
+        };
+        assert_eq!(s, de(&ser(s.clone()).unwrap()).unwrap())
+    }
+
+    #[test]
+    fn enum1_roundtrips() {
+        let e11 = Enum1::CaseStr("bye".into());
+        let e12 = Enum1::CaseInt(11);
+        let e13 = Enum1::CaseFF(Fq::double(&Fq::double(&Fq::ONE)));
+        for e in [e11, e12, e13] {
+            assert_eq!(e, de(&ser(e.clone()).unwrap()).unwrap());
+        }
+    }
+
+    /// An enum can be deserialized to another, if the enum constructor has the
+    /// same index and uses the same inner data
+    #[test]
+    fn stable_enum() {
+        #[derive(PartialEq, Debug, Clone, Serialize, Deserialize)]
+        enum Enum2<F: LurkField> {
+            CaseStr(String),
+            CaseInt(i32),
+            CaseFF(F),
+            Foo,
+        }
+
+        impl<F: LurkField> HasFieldModulus for Enum2<F> {
+            fn field_modulus() -> String {
+                F::MODULUS.to_string()
+            }
+        }
+        let e11 = Enum1::CaseStr("bye".into());
+        let e12 = Enum1::CaseInt(11);
+        let e13 = Enum1::CaseFF(Fq::double(&Fq::double(&Fq::ONE)));
+
+        let e21 = Enum2::CaseStr("bye".into());
+        let e22 = Enum2::CaseInt(11);
+        let e23 = Enum2::CaseFF(Fq::double(&Fq::double(&Fq::ONE)));
+
+        for (e1, e2) in [(e11, e21), (e12, e22), (e13, e23)] {
+            assert_eq!(e2.clone(), de(&ser(e1.clone()).unwrap()).unwrap());
+            assert_eq!(e1, de(&ser(e2).unwrap()).unwrap());
+        }
     }
 }
