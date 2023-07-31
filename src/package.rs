aa949b4cf86ce5ca7e73cada5709e3c2afbddff2
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-07-31 18:19:40 -0300

    second version

--- src/package.rs
+++ src/package.rs
@@ -5,7 +5,7 @@ use crate::Symbol;
 pub struct Package {
     name: Symbol,
     interned: HashSet<String>,
-    mappings: HashMap<String, Symbol>,
+    resolved: HashMap<String, Symbol>,
 }
 
 impl Package {
@@ -14,7 +14,7 @@ impl Package {
         Self {
             name,
             interned: Default::default(),
-            mappings: Default::default(),
+            resolved: Default::default(),
         }
     }
 
@@ -30,53 +30,32 @@ impl Package {
 
     #[inline]
     pub fn resolve(&self, symbol_name: &str) -> Option<&Symbol> {
-        self.mappings.get(symbol_name)
+        self.resolved.get(symbol_name)
     }
 
     pub fn intern(&mut self, symbol_name: &str) -> &Symbol {
         if self.interned.contains(symbol_name) {
-            self.resolve(&symbol_name)
+            self.resolve(symbol_name)
                 .expect("symbol must have been interned")
         } else {
             let symbol_name_clone = symbol_name.to_string();
             let symbol = self.name.direct_child(symbol_name_clone.clone());
             self.interned.insert(symbol_name_clone.clone());
-            self.mappings.insert(symbol_name_clone, symbol.clone());
+            self.resolved.insert(symbol_name_clone, symbol);
             self.resolve(symbol_name).unwrap()
         }
     }
 
-    pub fn import_symbols(&mut self, package_name: &Symbol, symbol_names: &[String]) {
+    pub fn import_symbols(&mut self, package_name: &Symbol, symbol_names: &HashSet<String>) {
         symbol_names.iter().for_each(|symbol_name| {
             let symbol_name = symbol_name.to_string();
             let symbol = package_name.direct_child(symbol_name.clone());
-            self.mappings.insert(symbol_name, symbol);
+            self.resolved.insert(symbol_name, symbol);
         })
     }
 
-    // pub fn use_package(&mut self, )
-
-    // #[inline]
-    // pub fn intern(&mut self, symbol_name: String) {
-    //     self.interned.insert(symbol_name);
-    // }
-
-    // pub fn import_symbols(&mut self, package_name: Symbol, symbol_names: &[String]) {
-    //     match self.imported.get_mut(&package_name) {
-    //         None => {
-    //             let set = symbol_names
-    //                 .iter()
-    //                 .fold(HashSet::default(), |mut acc, symbol_name| {
-    //                     acc.insert(symbol_name.clone());
-    //                     acc
-    //                 });
-    //             self.imported.insert(package_name, set);
-    //         }
-    //         Some(pkg) => {
-    //             symbol_names.iter().for_each(|symbol_name| {
-    //                 pkg.insert(symbol_name.clone());
-    //             });
-    //         }
-    //     }
-    // }
+    #[inline]
+    pub fn use_package(&mut self, package: &Self) {
+        self.import_symbols(&package.name, &package.interned);
+    }
 }
