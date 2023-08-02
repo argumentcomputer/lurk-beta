ef89620cb10abae72b1964429f7af75453c864db
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-08-02 17:02:52 -0300

    initiating the default package manager

--- src/packaging/manager.rs
+++ src/packaging/manager.rs
@@ -57,3 +57,67 @@ impl PackageManager {
         self.get_current().format(symbol)
     }
 }
+
+impl Default for PackageManager {
+    fn default() -> Self {
+        let root_package_name = SymbolRef::new(crate::Symbol::root());
+        let mut root_package = Package::new(root_package_name);
+
+        // bootstrap the keyword package
+        let keyword_package_name = root_package.intern("keyword".into());
+        let keyword_package = Package::new(keyword_package_name);
+
+        // bootstrap the lurk package
+        let lurk_package_name = root_package.intern("lurk".into());
+        let mut lurk_package = Package::new(lurk_package_name);
+        LURK_PACKAGE_SYMBOLS_NAMES.iter().for_each(|symbol_name| {
+            lurk_package.intern(symbol_name.to_string());
+        });
+
+        // initiate the package manager with the root package and add the keyword
+        // and the lurk packages
+        let mut manager = Self::init(root_package);
+        manager.add_package(keyword_package);
+        manager.add_package(lurk_package);
+        manager
+    }
+}
+
+const LURK_PACKAGE_SYMBOLS_NAMES: [&str; 36] = [
+    "atom",
+    "begin",
+    "car",
+    "cdr",
+    "char",
+    "comm",
+    "commit",
+    "cons",
+    "current-env",
+    "emit",
+    "eval",
+    "eq",
+    "hide",
+    "if",
+    "lambda",
+    "let",
+    "letrec",
+    "nil",
+    "num",
+    "u64",
+    "open",
+    "quote",
+    "secret",
+    "strcons",
+    "t",
+    "+",
+    "-",
+    "*",
+    "/",
+    "%",
+    "=",
+    "<",
+    ">",
+    "<=",
+    ">=",
+    "_",
+];
--- src/packaging/mod.rs
+++ src/packaging/mod.rs
@@ -1,3 +1,2 @@
-pub mod lurk;
 pub mod manager;
 pub mod package;
