612e54d53c1a11648e777a6079261effac4a39c8
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-08-31 15:14:10 -0300

    fix hashing of symbols (#636)
    * fix hashing of symbols
    
    * add test for the correctness of hashing symbols

--- tests/lurk-cli-tests.rs
+++ tests/lurk-cli-tests.rs
@@ -98,7 +98,7 @@ fn test_prove_and_verify() {
 
     let mut file = File::create(lurk_file.clone()).unwrap();
     file.write_all(b"!(prove (+ 1 1))\n").unwrap();
-    file.write_all(b"!(verify \"Nova_Pallas_10_0d723f6dd68729d7d119a13386f81d04daf6e1715f9ad53fb1ea54646771108a\")\n").unwrap();
+    file.write_all(b"!(verify \"Nova_Pallas_10_3f2526abf20fc9006dd93c0d3ff49954ef070ef52d2e88426974de42cc27bdb2\")\n").unwrap();
 
     let mut cmd = lurk_cmd();
     cmd.arg("load");
