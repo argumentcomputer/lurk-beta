0d2088b3d0efbd7c0622c700179ab81c0a2878b1
Lars Kuhtz <lars@kadena.io>
2023-09-17 21:54:16 -0700

    cargo fmt

--- src/cli/repl/meta_cmd.rs
+++ src/cli/repl/meta_cmd.rs
@@ -1,43 +1,38 @@
-use std::process;
-use camino::Utf8Path;
 use anyhow::{bail, Context, Result};
+use camino::Utf8Path;
+use std::process;
 
 use crate::{
     cli::lurk_proof::LurkProof,
+    field::LurkField,
     lurk_sym_ptr,
     package::{Package, SymbolRef},
     ptr::Ptr,
-    writer::Write,
     tag::{ContTag, ExprTag},
-    field::LurkField,
+    writer::Write,
 };
 
 use super::Repl;
 
-pub(super) struct MetaCmd<F: LurkField>
-{
+pub(super) struct MetaCmd<F: LurkField> {
     name: &'static str,
     summary: &'static str,
     format: &'static str,
     description: &'static [&'static str],
     example: &'static [&'static str],
-    pub(super) run: fn(repl: &mut Repl<F>, cmd: &str, args: &Ptr<F>, pwd_path: &Utf8Path) -> Result<()>,
+    pub(super) run:
+        fn(repl: &mut Repl<F>, cmd: &str, args: &Ptr<F>, pwd_path: &Utf8Path) -> Result<()>,
 }
 
 type F = pasta_curves::pallas::Scalar; // TODO: generalize this
 
 impl MetaCmd<F> {
     const LOAD: MetaCmd<F> = MetaCmd {
-        name:
-            "load",
-        summary:
-            "Load lurk expressions from a file path.",
-        format:
-            "!(load <string>)",
+        name: "load",
+        summary: "Load lurk expressions from a file path.",
+        format: "!(load <string>)",
         description: &[],
-        example: &[
-            "Load lurk expressions from a file path."
-        ],
+        example: &["Load lurk expressions from a file path."],
         run: |repl, cmd, args, pwd_path| {
             let first = repl.peek1(cmd, args)?;
             match repl.store.fetch_string(&first) {
@@ -48,25 +43,20 @@ impl MetaCmd<F> {
                 _ => bail!("Argument of `load` must be a string."),
             }
             return Ok(());
-        }
+        },
     };
 }
 
 impl MetaCmd<F> {
     const DEF: MetaCmd<F> = MetaCmd {
-        name:
-            "def",
-        summary:
-            "Extends env with a non-recursive binding.",
-        format:
-            "!(def <binding> <body>)",
+        name: "def",
+        summary: "Extends env with a non-recursive binding.",
+        format: "!(def <binding> <body>)",
         description: &[
             "Gets macroexpanded to this: (let ((foo (lambda () 123))) (current-env))",
             "The state's env is set to the result.",
         ],
-        example: &[
-            "!(def foo (lambda () 123))"
-        ],
+        example: &["!(def foo (lambda () 123))"],
         run: |repl: &mut Repl<F>, cmd: &str, args: &Ptr<F>, _pwd_path: &Utf8Path| {
             let (first, second) = repl.peek2(cmd, args)?;
             let l = lurk_sym_ptr!(&repl.store, let_);
@@ -90,12 +80,9 @@ impl MetaCmd<F> {
 
 impl MetaCmd<F> {
     const DEFREC: MetaCmd<F> = MetaCmd {
-        name:
-            "defrec",
-        summary:
-            "Extends the env with a recursive binding.",
-        format:
-            "!(defrec <binding> <body>)",
+        name: "defrec",
+        summary: "Extends the env with a recursive binding.",
+        format: "!(defrec <binding> <body>)",
         description: &[
             "Gets macroexpanded to this: (letrec ((foo (lambda () 123))) (current-env))",
             "The state's env is set ot the result.",
@@ -124,23 +111,17 @@ impl MetaCmd<F> {
                 new_name.fmt_to_string(&repl.store, &repl.state.borrow())
             );
             return Ok(());
-        }
+        },
     };
- }
+}
 
 impl MetaCmd<F> {
     const ASSERT: MetaCmd<F> = MetaCmd {
-        name:
-            "assert",
-        summary:
-            "Assert that an expression evaluates to true.",
-        format:
-            "!(assert <expr>)",
+        name: "assert",
+        summary: "Assert that an expression evaluates to true.",
+        format: "!(assert <expr>)",
         description: &[],
-        example: &[
-            "!(assert t)",
-            "!(assert (eq 3 (+ 1 2)))",
-        ],
+        example: &["!(assert t)", "!(assert (eq 3 (+ 1 2)))"],
         run: |repl, cmd, args, _pwd_path| {
             let first = repl.peek1(cmd, args)?;
             let (first_io, ..) = repl.eval_expr(first)?;
@@ -158,16 +139,11 @@ impl MetaCmd<F> {
 
 impl MetaCmd<F> {
     const ASSERT_EQ: MetaCmd<F> = MetaCmd {
-        name:
-            "assert-eq",
-        summary:
-            "Assert that two expressions evaluate to the same value.",
-        format:
-            "!(assert-eq <expr> <expr>)",
+        name: "assert-eq",
+        summary: "Assert that two expressions evaluate to the same value.",
+        format: "!(assert-eq <expr> <expr>)",
         description: &[],
-        example: &[
-            "!(assert-eq 3 (+ 1 2))"
-        ],
+        example: &["!(assert-eq 3 (+ 1 2))"],
         run: |repl, cmd, args, _pwd_path| {
             let (first, second) = repl.peek2(cmd, args)?;
             let (first_io, ..) = repl
@@ -236,16 +212,11 @@ impl MetaCmd<F> {
 
 impl MetaCmd<F> {
     const ASSERT_ERROR: MetaCmd<F> = MetaCmd {
-        name:
-            "assert-error",
-        summary:
-            "Assert that a evaluation of <expr> fails.",
-        format:
-            "!(assert-error <expr>)",
+        name: "assert-error",
+        summary: "Assert that a evaluation of <expr> fails.",
+        format: "!(assert-error <expr>)",
         description: &[],
-        example: &[
-            "!(assert-error (1 1))"
-        ],
+        example: &["!(assert-error (1 1))"],
         run: |repl, cmd, args, _pwd_path| {
             let first = repl.peek1(cmd, args)?;
             let (first_io, ..) = repl.eval_expr_allowing_error_continuation(first)?;
@@ -285,12 +256,9 @@ impl MetaCmd<F> {
 
 impl MetaCmd<F> {
     const HIDE: MetaCmd<F> = MetaCmd {
-        name:
-            "hide",
-        summary:
-            "Return and persist the commitment of <exp> using secret <secret>.",
-        format:
-            "!(hide <secret> <expr>)",
+        name: "hide",
+        summary: "Return and persist the commitment of <exp> using secret <secret>.",
+        format: "!(hide <secret> <expr>)",
         description: &[],
         example: &[
             "!(hide 12345 '(13 . 21))",
@@ -313,18 +281,15 @@ impl MetaCmd<F> {
             };
             repl.hide(secret.into_scalar(), second_io.expr)?;
             return Ok(());
-        }
+        },
     };
 }
 
 impl MetaCmd<F> {
     const FETCH: MetaCmd<F> = MetaCmd {
-        name:
-            "fetch",
-        summary:
-            "Add data from a commitment to the repl store.",
-        format:
-            "!(fetch <commitment>)",
+        name: "fetch",
+        summary: "Add data from a commitment to the repl store.",
+        format: "!(fetch <commitment>)",
         description: &[],
         example: &[
             "!(commit '(13 . 21))",
@@ -334,18 +299,15 @@ impl MetaCmd<F> {
             let hash = repl.get_comm_hash(cmd, args)?;
             repl.fetch(&hash, false)?;
             return Ok(());
-        }
+        },
     };
 }
 
 impl MetaCmd<F> {
     const OPEN: MetaCmd<F> = MetaCmd {
-        name:
-            "open",
-        summary:
-            "Open a commitment.",
-        format:
-            "!(open <commitment>)",
+        name: "open",
+        summary: "Open a commitment.",
+        format: "!(open <commitment>)",
         description: &[],
         example: &[
             "!(commit '(13 . 21))",
@@ -355,52 +317,38 @@ impl MetaCmd<F> {
             let hash = repl.get_comm_hash(cmd, args)?;
             repl.fetch(&hash, true)?;
             return Ok(());
-        }
+        },
     };
 }
 
 impl<F: LurkField> MetaCmd<F> {
     const CLEAR: MetaCmd<F> = MetaCmd {
-        name:
-            "clear",
-        summary:
-            "Reset the current environment to be empty.",
-        format:
-            "!(clear)",
+        name: "clear",
+        summary: "Reset the current environment to be empty.",
+        format: "!(clear)",
         description: &[],
-        example: &[
-            "!(def a 1)",
-            "(current-env)",
-            "!(clear)",
-            "(current-env)",
-        ],
+        example: &["!(def a 1)", "(current-env)", "!(clear)", "(current-env)"],
         run: |repl, _cmd, _args, _pwd_path| {
             repl.env = lurk_sym_ptr!(&repl.store, nil);
             return Ok(());
-        }
+        },
     };
 }
 
 impl MetaCmd<F> {
     const SET_ENV: MetaCmd<F> = MetaCmd {
-        name:
-            "set-env",
-        summary:
-            "Set the env to the result of evaluating the first argument.",
-        format:
-            "!(set-env <expr>)",
+        name: "set-env",
+        summary: "Set the env to the result of evaluating the first argument.",
+        format: "!(set-env <expr>)",
         description: &[],
-        example: &[
-            "!(set-env '((a . 1) (b . 2)))",
-            "a",
-        ],
+        example: &["!(set-env '((a . 1) (b . 2)))", "a"],
         run: |repl, cmd, args, _pwd_path| {
             // The state's env is set to the result of evaluating the first argument.
             let first = repl.peek1(cmd, args)?;
             let (first_io, ..) = repl.eval_expr(first)?;
             repl.env = first_io.expr;
             return Ok(());
-        }
+        },
     };
 }
 
@@ -457,12 +405,9 @@ impl<F: LurkField> MetaCmd<F> {
 
 impl<F: LurkField> MetaCmd<F> {
     const DEFPACKAGE: MetaCmd<F> = MetaCmd {
-        name:
-            "defpackage",
-        summary:
-            "Add a package to the state.",
-        format:
-            "!(defpackage <string|symbol>)",
+        name: "defpackage",
+        summary: "Add a package to the state.",
+        format: "!(defpackage <string|symbol>)",
         description: &[],
         example: &["!(defpackage abc)"],
         run: |repl, _cmd, args, _pwd_path| {
@@ -477,18 +422,15 @@ impl<F: LurkField> MetaCmd<F> {
             let package = Package::new(name);
             repl.state.borrow_mut().add_package(package);
             return Ok(());
-        }
+        },
     };
 }
 
 impl<F: LurkField> MetaCmd<F> {
     const IMPORT: MetaCmd<F> = MetaCmd {
-        name:
-            "import",
-        summary:
-            "Import a single or several packages.",
-        format:
-            "!(import <string|package> ...)",
+        name: "import",
+        summary: "Import a single or several packages.",
+        format: "!(import <string|package> ...)",
         description: &[],
         example: &[],
         run: |repl, _cmd, args, _pwd_path| {
@@ -513,18 +455,15 @@ impl<F: LurkField> MetaCmd<F> {
                 repl.state.borrow_mut().import(&symbols_vec)?;
             }
             return Ok(());
-        }
+        },
     };
 }
 
 impl<F: LurkField> MetaCmd<F> {
     const IN_PACKAGE: MetaCmd<F> = MetaCmd {
-        name:
-            "in-package",
-        summary:
-            "set the current package.",
-        format:
-            "!(in-package <string|symbol>)",
+        name: "in-package",
+        summary: "set the current package.",
+        format: "!(in-package <string|symbol>)",
         description: &[],
         example: &[
             "!(defpackage abc)",
@@ -553,27 +492,20 @@ impl<F: LurkField> MetaCmd<F> {
                 ),
             }
             return Ok(());
-        }
+        },
     };
 }
 
 impl<F: LurkField> MetaCmd<F> {
     const HELP: MetaCmd<F> = MetaCmd {
-        name:
-            "help",
-        summary:
-            "Print help message.",
-        format:
-            "!(help [<string>|<symbol>])",
+        name: "help",
+        summary: "Print help message.",
+        format: "!(help [<string>|<symbol>])",
         description: &[
             "Without arguments it prints a summary of all available commands.",
             "Otherwise the full help for the command in the first argument is printed.",
         ],
-        example: &[
-            "!(help)",
-            "!(help verify)",
-            "!(help \"load\")",
-        ],
+        example: &["!(help)", "!(help verify)", "!(help \"load\")"],
         run: |repl, cmd, args, _pwd_path| {
             let first = repl.peek1(cmd, args)?;
             match first.tag {
@@ -592,11 +524,11 @@ impl<F: LurkField> MetaCmd<F> {
                     for (_, i) in MetaCmd::cmds().iter().sorted_by_key(|x| x.0) {
                         println!("  {} - {}", i.name, i.summary);
                     }
-                },
+                }
                 _ => bail!("The optional argument of `help` must be a string or symbol"),
             }
             return Ok(());
-        }
+        },
     };
 
     fn meta_help(cmd: &str) {
@@ -613,38 +545,36 @@ impl<F: LurkField> MetaCmd<F> {
                 for &e in i.example.iter() {
                     println!("    {}", e);
                 }
-            },
+            }
             None => println!("unknown command {}", cmd),
         }
     }
 }
 
 impl MetaCmd<F> {
-
     const CMDS: [MetaCmd<F>; 19] = [
-            MetaCmd::LOAD,
-            MetaCmd::DEF,
-            MetaCmd::DEFREC,
-            MetaCmd::ASSERT,
-            MetaCmd::ASSERT_EQ,
-            MetaCmd::ASSERT_EMITTED,
-            MetaCmd::ASSERT_ERROR,
-            MetaCmd::COMMIT,
-            MetaCmd::HIDE,
-            MetaCmd::FETCH,
-            MetaCmd::OPEN,
-            MetaCmd::CLEAR,
-            MetaCmd::SET_ENV,
-            MetaCmd::PROVE,
-            MetaCmd::VERIFY,
-            MetaCmd::DEFPACKAGE,
-            MetaCmd::IMPORT,
-            MetaCmd::IN_PACKAGE,
-            MetaCmd::HELP,
+        MetaCmd::LOAD,
+        MetaCmd::DEF,
+        MetaCmd::DEFREC,
+        MetaCmd::ASSERT,
+        MetaCmd::ASSERT_EQ,
+        MetaCmd::ASSERT_EMITTED,
+        MetaCmd::ASSERT_ERROR,
+        MetaCmd::COMMIT,
+        MetaCmd::HIDE,
+        MetaCmd::FETCH,
+        MetaCmd::OPEN,
+        MetaCmd::CLEAR,
+        MetaCmd::SET_ENV,
+        MetaCmd::PROVE,
+        MetaCmd::VERIFY,
+        MetaCmd::DEFPACKAGE,
+        MetaCmd::IMPORT,
+        MetaCmd::IN_PACKAGE,
+        MetaCmd::HELP,
     ];
 
     pub(super) fn cmds() -> std::collections::HashMap<&'static str, MetaCmd<F>> {
         return std::collections::HashMap::from(Self::CMDS.map(|x| (x.name, x)));
     }
 }
-
