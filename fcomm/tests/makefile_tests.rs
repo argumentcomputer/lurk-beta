46e56cfc105d05aba5b81b9df6fc90a14446cc17
François Garillot <francois@garillot.net>
2023-10-12 09:56:33 -0400

    test: repair makefile tests

--- fcomm/tests/makefile_tests.rs
+++ fcomm/tests/makefile_tests.rs
@@ -16,6 +16,19 @@ fn test_make_fcomm_examples() {
         "Failed to find the fcomm examples directory"
     );
 
+    // Make clean
+    let make_clean_output = Command::new("make")
+        .arg("clean")
+        .current_dir(examples_dir)
+        .output()
+        .expect("Failed to run the make command, is make installed?");
+
+    assert!(
+        make_clean_output.status.success(),
+        "Make command exited with an error: {}",
+        String::from_utf8_lossy(&make_clean_output.stderr)
+    );
+
     // Run the make command in the examples directory
     let cpus = num_cpus::get();
 
