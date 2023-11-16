6ca3c1466a999a043704076cb8096e3b40e882fd
François Garillot <francois@garillot.net>
2023-11-15 22:12:38 -0500

    refactor: Remove unused utility `ToInputs`
    - Removed the `ToInputs` trait from the codebase, impacting `circuit/mod.rs` file.
    - Cleaned up unused imports.

--- src/circuit/mod.rs
+++ src/circuit/mod.rs
@@ -1,26 +1,4 @@
-use crate::field::LurkField;
-
-use crate::store::Store;
-
 #[macro_use]
 pub mod gadgets;
 
 pub mod circuit_frame;
-
-pub trait ToInputs<F: LurkField> {
-    fn to_inputs(&self, store: &Store<F>) -> Vec<F>;
-    fn input_size() -> usize;
-}
-
-impl<F: LurkField, T: ToInputs<F>> ToInputs<F> for Option<T> {
-    fn to_inputs(&self, store: &Store<F>) -> Vec<F> {
-        if let Some(t) = self {
-            t.to_inputs(store)
-        } else {
-            panic!("no inputs for None");
-        }
-    }
-    fn input_size() -> usize {
-        unimplemented!();
-    }
-}
