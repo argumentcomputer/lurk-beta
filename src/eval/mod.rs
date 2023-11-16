d34eacd1e2155d9987212576875571a162b6a147
François Garillot <francois@garillot.net>
2023-11-15 22:15:44 -0500

    refactor: Refactor `eval` module and remove `Status` enum
    - Removes `ContTag` import and the `Status` enum along with related methods and attributes from the `eval` module.

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -2,14 +2,8 @@ use crate::field::LurkField;
 use crate::hash_witness::{ConsWitness, ContWitness};
 use crate::ptr::{ContPtr, Ptr};
 use crate::store::Store;
-use crate::tag::ContTag;
 use crate::z_ptr::ZExprPtr;
 
-#[cfg(not(target_arch = "wasm32"))]
-use lurk_macros::serde_test;
-#[cfg(not(target_arch = "wasm32"))]
-use proptest_derive::Arbitrary;
-use serde::{Deserialize, Serialize};
 use std::cmp::PartialEq;
 use std::marker::PhantomData;
 
@@ -36,62 +30,6 @@ pub struct Frame<T: Copy, W: Copy, F: LurkField, C> {
     pub witness: W,
     pub meta: Meta<F>,
     pub _p: PhantomData<C>,
-}
-
-#[cfg_attr(not(target_arch = "wasm32"), derive(Arbitrary))]
-#[cfg_attr(not(target_arch = "wasm32"), serde_test)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
-pub enum Status {
-    Terminal,
-    Error,
-    Incomplete,
-}
-
-impl Default for Status {
-    fn default() -> Self {
-        Self::Incomplete
-    }
-}
-
-impl Status {
-    pub fn is_complete(&self) -> bool {
-        match self {
-            Self::Terminal | Self::Error => true,
-            Self::Incomplete => false,
-        }
-    }
-
-    pub fn is_terminal(&self) -> bool {
-        match self {
-            Self::Terminal => true,
-            Self::Incomplete | Self::Error => false,
-        }
-    }
-
-    pub fn is_error(&self) -> bool {
-        match self {
-            Self::Error => true,
-            Self::Terminal | Self::Incomplete => false,
-        }
-    }
-
-    pub fn to_cont<F: LurkField>(&self, s: &Store<F>) -> Option<ContPtr<F>> {
-        match self {
-            Self::Terminal => Some(s.intern_cont_terminal()),
-            Self::Error => Some(s.intern_cont_error()),
-            Self::Incomplete => None,
-        }
-    }
-}
-
-impl<F: LurkField> From<ContPtr<F>> for Status {
-    fn from(cont: ContPtr<F>) -> Self {
-        match cont.tag {
-            ContTag::Terminal => Self::Terminal,
-            ContTag::Error => Self::Error,
-            _ => Self::Incomplete,
-        }
-    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
