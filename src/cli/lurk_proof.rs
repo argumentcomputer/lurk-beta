422b63d67afb8252b06a850dc473787e2bfe34fd
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-12-12 20:19:41 -0300

    drop pack and unpack commands, made obsolete by #942

--- src/cli/lurk_proof.rs
+++ src/cli/lurk_proof.rs
@@ -4,7 +4,6 @@ use ::nova::{
 };
 use abomonation::Abomonation;
 use anyhow::{bail, Result};
-use camino::Utf8PathBuf;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::{collections::HashMap, sync::Arc};
 
@@ -44,30 +43,6 @@ pub(crate) struct LurkProofMeta<F: LurkField> {
 impl<F: LurkField> HasFieldModulus for LurkProofMeta<F> {
     fn field_modulus() -> String {
         F::MODULUS.to_owned()
-    }
-}
-
-impl<F: LurkField> LurkProofMeta<F> {
-    fn without_envs(self) -> Result<Self> {
-        if self.env_io.is_none() {
-            return Ok(self);
-        }
-        let Self {
-            iterations,
-            expr_io: (expr, expr_out),
-            env_io: _,
-            cont_io: (cont, cont_out),
-            z_dag,
-        } = self;
-        // creating a new `ZDag` without data from envs
-        let z_dag = z_dag.filtered(&[&expr, &expr_out, &cont, &cont_out])?;
-        Ok(Self {
-            iterations,
-            expr_io: (expr, expr_out),
-            env_io: None,
-            cont_io: (cont, cont_out),
-            z_dag,
-        })
     }
 }
 
@@ -187,59 +162,6 @@ where
 
 impl<
         'a,
-        F: CurveCycleEquipped,
-        C: Coprocessor<F> + Serialize + DeserializeOwned,
-        M: MultiFrameTrait<'a, F, C>,
-    > LurkProof<'a, F, C, M>
-where
-    <<E1<F> as Engine>::Scalar as ff::PrimeField>::Repr: Abomonation,
-    <<E2<F> as Engine>::Scalar as ff::PrimeField>::Repr: Abomonation,
-{
-    #[inline]
-    fn public_io(&self) -> (&[F], &[F]) {
-        match self {
-            Self::Nova {
-                proof: _,
-                public_inputs,
-                public_outputs,
-                ..
-            } => (public_inputs, public_outputs),
-        }
-    }
-
-    fn matches_meta(&self, meta: &LurkProofMeta<F>) -> bool {
-        let (public_input, public_output) = self.public_io();
-        let matches_exprs = {
-            let (expr, expr_out) = &meta.expr_io;
-            public_input[0] == expr.tag_field()
-                && &public_input[1] == expr.value()
-                && public_output[0] == expr_out.tag_field()
-                && &public_output[1] == expr_out.value()
-        };
-        let matches_envs = {
-            if let Some((env, env_out)) = &meta.env_io {
-                public_input[2] == env.tag_field()
-                    && &public_input[3] == env.value()
-                    && public_output[2] == env_out.tag_field()
-                    && &public_output[3] == env_out.value()
-            } else {
-                // no data to trigger inconsistency
-                true
-            }
-        };
-        let matches_conts = {
-            let (cont, cont_out) = &meta.cont_io;
-            public_input[4] == cont.tag_field()
-                && &public_input[5] == cont.value()
-                && public_output[4] == cont_out.tag_field()
-                && &public_output[5] == cont_out.value()
-        };
-        matches_exprs && matches_envs && matches_conts
-    }
-}
-
-impl<
-        'a,
         F: CurveCycleEquipped + Serialize,
         C: Coprocessor<F> + Serialize + DeserializeOwned,
         M: MultiFrameTrait<'a, F, C>,
@@ -298,90 +220,5 @@ where
                 Ok(proof.verify(&pp, public_inputs, public_outputs, *num_steps)?)
             }
         }
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(bound(serialize = "F: Serialize", deserialize = "F: DeserializeOwned"))]
-pub(crate) struct PackedLurkProof<
-    'a,
-    F: CurveCycleEquipped,
-    C: Coprocessor<F> + Serialize + DeserializeOwned,
-    M: MultiFrameTrait<'a, F, C>,
-> where
-    <<E1<F> as Engine>::Scalar as ff::PrimeField>::Repr: Abomonation,
-    <<E2<F> as Engine>::Scalar as ff::PrimeField>::Repr: Abomonation,
-{
-    proof: LurkProof<'a, F, C, M>,
-    meta: Option<LurkProofMeta<F>>,
-    key: String,
-}
-
-impl<
-        'a,
-        F: CurveCycleEquipped,
-        C: Coprocessor<F> + 'a + Serialize + DeserializeOwned,
-        M: MultiFrameTrait<'a, F, C>,
-    > HasFieldModulus for PackedLurkProof<'a, F, C, M>
-where
-    <<E1<F> as Engine>::Scalar as ff::PrimeField>::Repr: Abomonation,
-    <<E2<F> as Engine>::Scalar as ff::PrimeField>::Repr: Abomonation,
-{
-    fn field_modulus() -> String {
-        F::MODULUS.to_owned()
-    }
-}
-
-impl<
-        F: CurveCycleEquipped + Serialize + DeserializeOwned,
-        C: Coprocessor<F> + 'static + Serialize + DeserializeOwned,
-        M: MultiFrameTrait<'static, F, C>
-            + SuperStepCircuit<F>
-            + NonUniformCircuit<E1<F>, E2<F>, M, C2<F>>
-            + 'static,
-    > PackedLurkProof<'static, F, C, M>
-where
-    <<E1<F> as Engine>::Scalar as ff::PrimeField>::Repr: Abomonation,
-    <<E2<F> as Engine>::Scalar as ff::PrimeField>::Repr: Abomonation,
-{
-    pub(crate) fn pack(
-        proof_key: String,
-        path: &Utf8PathBuf,
-        exclude_meta: bool,
-        include_envs: bool,
-    ) -> Result<()> {
-        let proof: LurkProof<'_, F, C, M> = load(&proof_path(&proof_key))?;
-        let meta = if exclude_meta {
-            None
-        } else {
-            let meta: LurkProofMeta<F> = load(&proof_meta_path(&proof_key))?;
-            if include_envs {
-                Some(meta)
-            } else {
-                Some(meta.without_envs()?)
-            }
-        };
-        let packed_proof = PackedLurkProof {
-            proof,
-            meta,
-            key: proof_key,
-        };
-        dump(packed_proof, path)
-    }
-
-    pub(crate) fn unpack(path: &Utf8PathBuf) -> Result<()> {
-        let PackedLurkProof { proof, meta, key } = load::<Self>(path)?;
-        if !proof.verify()? {
-            bail!("Proof verification failed")
-        }
-        if let Some(meta) = meta {
-            if !proof.matches_meta(&meta) {
-                bail!("Meta data is incompatible with the proof")
-            }
-            meta.persist(&key)?;
-        }
-        proof.persist(&key)?;
-        println!("Proof {key} unpacked");
-        Ok(())
     }
 }
