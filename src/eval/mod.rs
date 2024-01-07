2611abf850e676f3506ed3471c4066f4cc2810b6
François Garillot <francois@garillot.net>
2024-01-07 15:45:14 -0500

    removing uneeded bound(s) on struct in src/eval/mod.rs

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -1,12 +1,10 @@
-use crate::field::LurkField;
-
 use std::cmp::PartialEq;
 use std::marker::PhantomData;
 
 pub mod lang;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct Frame<T: Copy, W: Copy, F: LurkField, C> {
+pub struct Frame<T: Copy, W: Copy, F, C> {
     pub input: T,
     pub output: T,
     pub i: usize,
