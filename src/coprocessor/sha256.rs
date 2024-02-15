cee455079d25080b9da6e5f569fdeea2587dc59a
Chris Pacia <ctpacia@gmail.com>
2024-02-15 13:02:47 -0500

    Update sha256 coprocessor discarded bits
    Previously, the pallas curve had a capacity of 254 bits. The new bn256
    curve has a capacity of 253 bits. This makes the pack_bits gadget used by
    sha256 coprocessor drop the three most significant bits of the output
    instead of just two.
    
    This commit updates the coprocessor's evaluate_simple implementation to
    set the correct number of bits to zero based on the field used to avoid a
    panic caused by a mismatch between the synthesize and evaluate outputs.

--- src/coprocessor/sha256.rs
+++ src/coprocessor/sha256.rs
@@ -79,11 +79,13 @@ fn compute_sha256<F: LurkField, T: Tag>(n: usize, z_ptrs: &[ZPtr<T, F>]) -> F {
 
     hasher.update(input);
     let mut bytes = hasher.finalize();
-    bytes.reverse();
-    let l = bytes.len();
-    // Discard the two most significant bits.
-    bytes[l - 1] &= 0b00111111;
 
+    // The pack_bits gadget used by the synthesize_sha256 function
+    // sets the n most significant bits of the hash output to zero,
+    // where n is 256 minus the field's capacity. We do the same
+    // here to match the output.
+    discard_bits::<F>(&mut bytes);
+    bytes.reverse();
     F::from_bytes(&bytes).unwrap()
 }
 
@@ -126,6 +128,22 @@ impl<F: LurkField> Sha256Coprocessor<F> {
             n,
             _p: Default::default(),
         }
+    }
+}
+
+// Retains the Scalar::CAPACITY last bits of a big-endian input
+fn discard_bits<Scalar: LurkField>(bytes: &mut [u8]) {
+    let bits_to_zero = 256 - Scalar::CAPACITY as usize;
+    let full_bytes_to_zero = bits_to_zero / 8;
+    let partial_bits_to_zero = bits_to_zero % 8;
+
+    for i in 0..full_bytes_to_zero {
+        bytes[i] = 0;
+    }
+
+    if partial_bits_to_zero > 0 {
+        let mask = 0xFF >> partial_bits_to_zero;
+        bytes[full_bytes_to_zero] &= mask;
     }
 }
 
