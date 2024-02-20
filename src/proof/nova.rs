cf6508864152b8c7fdbc31fb0590a2dca5e099c1
François Garillot <4142+huitseeker@users.noreply.github.com>
2024-02-20 09:29:32 -0500

    chore: Adapt to Arecibo changes (easy) (#1145)
    * chore: Adapt to Arecibo changes
    
    - Companion PR of https://github.com/lurk-lab/arecibo/pull/329
    
    * chore: point the repo back to dev

--- src/proof/nova.rs
+++ src/proof/nova.rs
@@ -205,7 +205,8 @@ pub fn public_params<'a, F: CurveCycleEquipped, C: Coprocessor<F> + 'a>(
         &circuit_secondary,
         &*commitment_size_hint1,
         &*commitment_size_hint2,
-    );
+    )
+    .unwrap();
     PublicParams {
         pp,
         pk_and_vk: OnceCell::new(),
